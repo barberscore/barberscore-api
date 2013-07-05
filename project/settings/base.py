8622c4024bbc2d6c3987d3e7f26ace8a117cfb6c
David Binetti <dbinetti@gmail.com>
2013-07-05 11:52:27 -0700

    Settings to turn on cacheing

--- project/settings/base.py
+++ project/settings/base.py
@@ -62,11 +62,13 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
 )
 
 ROOT_URLCONF = 'urls'
