29981f52d60e859de6506c3fdfc715cc10b43755
David Binetti <dbinetti@gmail.com>
2013-07-04 22:51:54 -0700

    Updates profile form

--- project/apps/common/forms.py
+++ project/apps/common/forms.py
@@ -10,4 +10,4 @@ from .models import (
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ('first_name', 'last_name', 'nickname', 'timezone')
+        fields = ('first_name', 'last_name', 'nickname', 'prediction')
