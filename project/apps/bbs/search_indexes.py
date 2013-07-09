a200adf1c9800fe51a6d3719c309a8f67967769a
David Binetti <dbinetti@gmail.com>
2013-07-09 00:24:20 -0700

    adding haystack

--- project/apps/bbs/search_indexes.py
+++ project/apps/bbs/search_indexes.py
@@ -1,20 +1,5 @@
 from haystack import indexes
-from .models import Singer, Contestant
-
-
-# class SingerIndex(indexes.ModelSearchIndex, indexes.Indexable):
-#     text = indexes.CharField(document=True, use_template=True)
-#     # # name = indexes.CharField(model_attr='name')
-
-#     # def get_model(self):
-#     #     return Singer
-
-#     # def index_queryset(self, using=None):
-#     #     """Used when the entire index for model is updated."""
-#     #     return self.get_model().objects.all()
-
-#     class Meta:
-#         model = Singer
+from .models import Contestant
 
 
 class ContestantIndex(indexes.ModelSearchIndex, indexes.Indexable):
--- project/settings/dev.py
+++ project/settings/dev.py
@@ -39,7 +39,7 @@ CACHES = {
 HAYSTACK_CONNECTIONS = {
     'default': {
         'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
-        'URL': 'http://127.0.0.1:9203/',
+        'URL': 'http://127.0.0.1:9200/',
         'INDEX_NAME': 'haystack',
     },
 }
