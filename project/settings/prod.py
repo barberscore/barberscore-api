5a25bec4df2f4e312c1cffd09c4aa784a706e9a8
David Binetti <dbinetti@gmail.com>
2013-07-10 01:18:50 -0700

    trying s3_folder_storage

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -10,22 +10,35 @@ AWS_ACCESS_KEY_ID = get_env_variable("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = get_env_variable("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = get_env_variable("AWS_STORAGE_BUCKET_NAME")
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+DEFAULT_FILE_STORAGE = 's3_folder_storage.s3.DefaultStorage'
+STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
+
+DEFAULT_S3_PATH = "media"
+STATIC_S3_PATH = "static"
+
 
 INSTALLED_APPS += (
     'storages',
     'raven.contrib.django.raven_compat',
+    's3_folder_storage',
 )
 
 RAVEN_CONFIG = {
     'dsn': get_env_variable("SENTRY_DSN"),
 }
 
-STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/static/'
+MEDIA_ROOT = "/{0}/".format(DEFAULT_S3_PATH)
+STATIC_ROOT = "/{0}/".format(STATIC_S3_PATH)
+
+STATIC_URL = 'http://' + AWS_STATIC_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/static/'
 MEDIA_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/media/'
 ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
+
+MEDIA_URL = '/media/'
+
+STATIC_URL = '/static/'
 
 ALLOWED_HOSTS = [get_env_variable("HEROKU_HOST")]
 
