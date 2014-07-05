49cd8da9ec75ac90b4fdaef0577760c20489365b
David Binetti <dbinetti@gmail.com>
2014-07-05 12:41:55 -0700

    Placement to admin

--- project/convention/admin.py
+++ project/convention/admin.py
@@ -88,6 +88,7 @@ class ContestantAdmin(admin.ModelAdmin):
             'fields': (
                 'name',
                 'contestant_type',
+                'placement',
             ),
         }),
         ('Details', {
