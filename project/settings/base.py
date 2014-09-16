d74ab019a371b46f8a2f92344f0509d8a2fde4f2
David Binetti <dbinetti@gmail.com>
2014-09-16 15:21:28 -0700

    Apps deactivate

--- project/settings/base.py
+++ project/settings/base.py
@@ -103,7 +103,7 @@ INSTALLED_APPS = (
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
-    'haystack',
+    # 'haystack',
     'convention',
     'noncense',
     'profile',
