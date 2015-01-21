ff3447695a6a613b3236d5f9599a7d543b6ff027
David Binetti <dbinetti@gmail.com>
2015-01-20 18:38:59 -0800

    Add bugsnag

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -1,1 +1,10 @@
 from .base import *
+
+BUGSNAG = {
+    "api_key": get_env_variable("BUGSNAG"),
+    "project_root": PROJECT_ROOT,
+}
+
+MIDDLEWARE_CLASSES += (
+    "bugsnag.django.middleware.BugsnagMiddleware",
+)
