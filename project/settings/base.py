c822c72ea8365312dcc42395fc561a79895ddb6c
David Binetti <dbinetti@gmail.com>
2015-01-20 22:39:59 -0800

    Add staticfiledirs setting

--- project/settings/base.py
+++ project/settings/base.py
@@ -36,10 +36,10 @@ LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 LOGIN_REDIRECT_URL = 'home'
 DOMAIN = get_env_variable("DOMAIN")
-# STATICFILES_DIRS = [os.path.join(
-#     PROJECT_ROOT,
-#     'website/static',
-# )]
+STATICFILES_DIRS = [os.path.join(
+    PROJECT_ROOT,
+    'apps/api/static',
+)]
 
 # Database
 DATABASE_URL = get_env_variable("DATABASE_URL")
