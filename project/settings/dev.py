2ff5194567ce36cf533435132895fde487977df7
David Binetti <dbinetti@gmail.com>
2015-01-27 09:32:47 -0800

    Configure logging

--- project/settings/dev.py
+++ project/settings/dev.py
@@ -3,3 +3,34 @@ from .base import *
 ALLOWED_HOSTS = [
     'localhost',
 ]
+
+LOGGING = {
+    'version': 1,
+    "disable_existing_loggers": True,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+    },
+    'formatters': {
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'loggers': {
+        'apps.api': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        },
+        'noncense': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        },
+        'utils': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        },
+    },
+}
