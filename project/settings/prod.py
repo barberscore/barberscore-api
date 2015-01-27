45cdf2f45be9e48c51cf8fcfd3e6f6a313f93be3
David Binetti <dbinetti@gmail.com>
2015-01-27 10:34:39 -0800

    Add bugsnag logging

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -13,3 +13,44 @@ ALLOWED_HOSTS = [
     get_env_variable("HEROKU_HOST"),
     '.barberscore.com',
 ]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'root': {
+        'level': 'ERROR',
+        'handlers': ['bugsnag'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(message)s'
+        },
+    },
+    'loggers': {
+        'api': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        },
+        'noncense': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        },
+        'utils': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        },
+    },
+    'handlers': {
+        'bugsnag': {
+            'level': 'ERROR',
+            'class': 'bugsnag.handlers.BugsnagHandler',
+            'formatter': 'verbose',
+        },
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+            'stream': sys.stdout,
+        },
+    },
+}
