4ec6d61c3224e7541f0d093a125af9050ffd41d6
David Binetti <dbinetti@gmail.com>
2015-01-31 04:13:48 -0800

    Turn off local media server for production

--- project/apps/website/urls.py
+++ project/apps/website/urls.py
@@ -36,4 +36,5 @@ urlpatterns = [
     url(r'^quartets/(?P<slug>[a-zA-Z0-9-]+)/$', views.quartet_detail, name='quartet-detail'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
