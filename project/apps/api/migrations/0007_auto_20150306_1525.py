c54b1b2366650a8f86ca078e90fd6d2c09c6c4ee
David Binetti <dbinetti@gmail.com>
2015-03-06 15:40:31 -0800

    Add Years

---
+++ project/apps/api/migrations/0007_auto_20150306_1525.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('api', '0006_auto_20150213_0928'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='convention',
+            name='year',
+            field=models.IntegerField(default=2015, choices=[(2000, 2000), (2001, 2001), (2002, 2002), (2003, 2003), (2004, 2004), (2005, 2005), (2006, 2006), (2007, 2007), (2008, 2008), (2009, 2009), (2010, 2010), (2011, 2011), (2012, 2012), (2013, 2013), (2014, 2014), (2015, 2015)]),
+        ),
+    ]
