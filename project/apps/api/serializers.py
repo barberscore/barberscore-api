3d3641d4d877310559e4687d3a19fee636183c06
David Binetti <dbinetti@gmail.com>
2015-05-01 15:09:53 -0700

    API and migration

--- project/apps/api/serializers.py
+++ project/apps/api/serializers.py
@@ -1,43 +1,130 @@
-# from rest_framework import serializers
+from rest_framework import serializers
 
-# from .models import (
-#     Singer,
-#     Chorus,
-#     Quartet,
-# )
-
-
-# class QuartetSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = Quartet
-#         lookup_field = 'slug'
-#         fields = (
-#             'url',
-#             'id',
-#             'name',
-#             'slug',
-#         )
+from .models import (
+    Singer,
+    Chorus,
+    Quartet,
+    Contest,
+    Convention,
+    Performance,
+    GroupFinish,
+)
 
 
-# class SingerSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = Singer
-#         lookup_field = 'slug'
-#         fields = (
-#             'url',
-#             'id',
-#             'name',
-#             'slug',
-#         )
+class SingerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Singer
+        lookup_field = 'slug'
+        fields = (
+            'url',
+            'id',
+            'name',
+            'slug',
+        )
 
 
-# class ChorusSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = Chorus
-#         lookup_field = 'slug'
-#         fields = (
-#             'url',
-#             'id',
-#             'name',
-#             'slug',
-#         )
+class ChorusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Chorus
+        lookup_field = 'slug'
+        fields = (
+            'url',
+            'id',
+            'name',
+            'slug',
+        )
+
+
+class QuartetSerializer(serializers.ModelSerializer):
+    singers = SingerSerializer(
+        many=True,
+        read_only=True,
+    )
+
+    class Meta:
+        model = Quartet
+        lookup_field = 'slug'
+        fields = (
+            'url',
+            'id',
+            'name',
+            'slug',
+            'singers',
+        )
+
+
+class GroupFinishSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = GroupFinish
+        fields = (
+            'seed',
+            'prelim',
+            'place',
+            'score',
+        )
+
+
+class PerformanceSerializer(serializers.ModelSerializer):
+    group = serializers.StringRelatedField(
+        read_only=True,
+    )
+
+    class Meta:
+        model = Performance
+        fields = (
+            'id',
+            'group',
+            'round',
+            'queue',
+            'session',
+            'stagetime',
+            'song1',
+            'song2',
+        )
+
+
+class ContestSerializer(serializers.ModelSerializer):
+    performances = PerformanceSerializer(
+        many=True,
+        read_only=True,
+    )
+    groupfinishes = GroupFinishSerializer(
+        many=True,
+        read_only=True,
+    )
+
+    kind = serializers.CharField(source='get_kind_display')
+
+    class Meta:
+        model = Contest
+        lookup_field = 'slug'
+
+        fields = (
+            'url',
+            'id',
+            'slug',
+            'panel',
+            'kind',
+            'year',
+            'performances',
+            'groupfinishes',
+        )
+
+
+class ConventionSerializer(serializers.ModelSerializer):
+    contests = ContestSerializer(
+        many=True,
+        read_only=True,
+    )
+
+    class Meta:
+        model = Convention
+        lookup_field = 'slug'
+        fields = (
+            'url',
+            'id',
+            'slug',
+            'contests',
+        )
