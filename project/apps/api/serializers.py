729ad4014d720519dfb1a2cb1a6d4e0641eecec8
David Binetti <dbinetti@gmail.com>
2015-05-05 11:01:46 -0700

    Flatten API

--- project/apps/api/serializers.py
+++ project/apps/api/serializers.py
@@ -15,111 +15,136 @@ class SingerSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Singer
-        lookup_field = 'slug'
+        # lookup_field = 'slug'
         fields = (
-            'url',
             'id',
+            'url',
             'name',
             'slug',
-        )
-
-
-class ChorusSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Chorus
-        lookup_field = 'slug'
-        fields = (
-            'url',
-            'id',
-            'name',
-            'slug',
-        )
-
-
-class QuartetSerializer(serializers.ModelSerializer):
-    singers = SingerSerializer(
-        many=True,
-        read_only=True,
-    )
-
-    class Meta:
-        model = Quartet
-        lookup_field = 'slug'
-        fields = (
-            'url',
-            'id',
-            'name',
-            'slug',
-            'singers',
         )
 
 
 class PerformanceSerializer(serializers.ModelSerializer):
-    group = serializers.StringRelatedField(
-        read_only=True,
+    round = serializers.CharField(
+        source='get_round_display',
     )
 
     class Meta:
         model = Performance
         fields = (
             'id',
-            'group',
+            'url',
             'round',
             'queue',
             'session',
             'stagetime',
+            'place',
             'song1',
+            'mus1',
+            'prs1',
+            'sng1',
             'song2',
+            'mus2',
+            'prs2',
+            'sng2',
+            'men',
         )
 
 
 class ContestantSerializer(serializers.ModelSerializer):
+    performances = serializers.PrimaryKeyRelatedField(
+        many=True,
+        read_only=True,
+    )
+
+    group = serializers.StringRelatedField(
+        read_only=True,
+    )
 
     class Meta:
         model = Contestant
         fields = (
+            'id',
+            'url',
+            'group',
             'seed',
             'prelim',
             'place',
             'score',
+            'performances',
         )
 
 
-class ContestSerializer(serializers.ModelSerializer):
+class ChorusSerializer(serializers.ModelSerializer):
     contestants = ContestantSerializer(
         many=True,
         read_only=True,
     )
 
-    kind = serializers.CharField(source='get_kind_display')
-
     class Meta:
-        model = Contest
-        lookup_field = 'slug'
-
+        model = Chorus
+        # lookup_field = 'slug'
         fields = (
-            'url',
             'id',
+            'url',
+            'name',
             'slug',
-            'panel',
-            'kind',
-            'year',
             'contestants',
         )
 
 
-class ConventionSerializer(serializers.ModelSerializer):
+class ContestSerializer(serializers.ModelSerializer):
+    contestants = serializers.PrimaryKeyRelatedField(
+        many=True,
+        read_only=True,
+    )
+
+    kind = serializers.CharField(
+        source='get_kind_display',
+    )
+
+    class Meta:
+        model = Contest
+        fields = (
+            'id',
+            'url',
+            'kind',
+            'panel',
+            'scoresheet_pdf',
+            'contestants',
+        )
+
+
+class QuartetSerializer(serializers.ModelSerializer):
     contests = ContestSerializer(
+        many=True,
+        read_only=True,
+        source='contestant.contests'
+    )
+
+    class Meta:
+        model = Quartet
+        # lookup_field = 'slug'
+        fields = (
+            'id',
+            'url',
+            'name',
+            'slug',
+            'contests',
+        )
+
+
+class ConventionSerializer(serializers.ModelSerializer):
+    contests = serializers.PrimaryKeyRelatedField(
         many=True,
         read_only=True,
     )
 
     class Meta:
         model = Convention
-        lookup_field = 'slug'
+        # lookup_field = 'slug'
         fields = (
-            'url',
             'id',
-            'slug',
+            'url',
             'contests',
         )
