090da095469fa1d98a7e8c035b6d5a85096801d1
David Binetti <dbinetti@gmail.com>
2015-05-08 06:27:20 -0700

    Add level to contest

---
+++ project/apps/api/migrations/0034_auto_20150508_0627.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('api', '0033_auto_20150508_0602'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='contest',
+            name='level',
+            field=models.IntegerField(default=1, choices=[(1, b'International'), (2, b'District')]),
+        ),
+        migrations.AlterField(
+            model_name='group',
+            name='district_tag',
+            field=models.IntegerField(default=0, choices=[(0, b'BHS'), (1, b'CAR'), (2, b'CSD'), (3, b'DIX'), (4, b'EVG'), (5, b'FWD'), (6, b'ILL'), (7, b'JAD'), (8, b'LOL'), (9, b'MAD'), (10, b'NED'), (11, b'NSC'), (12, b'ONT'), (13, b'PIO'), (14, b'RMD'), (15, b'SLD'), (16, b'SUN'), (17, b'SWD'), (18, b'BABS'), (19, b'BHA'), (20, b'BHNZ'), (21, b'BING'), (22, b'DABS'), (23, b'FABS'), (24, b'IABS'), (25, b'NZABS'), (26, b'SABS'), (27, b'SNOBS'), (28, b'SPATS')]),
+        ),
+    ]
