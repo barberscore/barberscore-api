8253a5f12259077d2a48b9e3632f436cebe39460
David Binetti <dbinetti@gmail.com>
2015-05-09 02:04:57 -0700

    Refactor serializers

--- project/apps/api/urls.py
+++ project/apps/api/urls.py
@@ -1,25 +1,15 @@
 from rest_framework import routers
 
 from .views import (
-    SingerViewSet,
+    ConventionViewSet,
     ChorusViewSet,
     QuartetViewSet,
-    ConventionViewSet,
-    ContestViewSet,
-    DistrictViewSet,
-    PerformanceViewSet,
-    ContestantGroupViewSet,
 )
 
 router = routers.SimpleRouter()
-router.register(r'singers', SingerViewSet)
-router.register(r'choruses', ChorusViewSet)
-router.register(r'contests', ContestViewSet)
-router.register(r'performances', PerformanceViewSet)
-router.register(r'districts', DistrictViewSet)
-router.register(r'quartets', QuartetViewSet)
-router.register(r'conventions', ConventionViewSet)
-router.register(r'contestants', ContestantGroupViewSet)
 
+router.register(r'conventions', ConventionViewSet)
+router.register(r'choruses', ChorusViewSet)
+router.register(r'quartets', QuartetViewSet)
 
 urlpatterns = router.urls
