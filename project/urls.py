57746e03f79e27aa2e7bb017dddcabaf5ad29e57
David Binetti <dbinetti@gmail.com>
2015-05-12 09:17:43 -0700

    Rename FK

--- project/urls.py
+++ project/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     # url(r'^', include('noncense.urls', namespace='noncense')),
-    url(r'^', include('apps.website.urls', namespace='website')),
+    # url(r'^', include('apps.website.urls', namespace='website')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('apps.api.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
