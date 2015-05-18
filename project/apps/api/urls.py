5a84db470e26ac26db4d56ae443bdc3a884768aa
David Binetti <dbinetti@gmail.com>
2015-05-18 14:18:59 -0700

    Embedded

--- project/apps/api/urls.py
+++ project/apps/api/urls.py
@@ -7,6 +7,7 @@ from .views import (
     ContestantViewSet,
     PerformanceViewSet,
     ScheduleViewSet,
+    ScoreViewSet,
     # SearchViewSet,
 )
 
@@ -16,8 +17,9 @@ router.register(r'conventions', ConventionViewSet)
 router.register(r'contests', ContestViewSet)
 router.register(r'groups', GroupViewSet)
 router.register(r'contestants', ContestantViewSet)
+# router.register(r'schedules', ScheduleViewSet)
 router.register(r'performances', PerformanceViewSet)
-router.register(r'schedules', ScheduleViewSet)
+# router.register(r'scores', ScoreViewSet)
 # router.register(r'search', SearchViewSet, base_name='search')
 
 urlpatterns = router.urls
