7d26609bcf01053c6d41768f637fe686afb1d752
David Binetti <dbinetti@gmail.com>
2015-06-12 14:49:10 -0700

    Reduce dependencies and remove cruft.

--- project/apps/api/config.py
+++ project/apps/api/config.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
-import watson
 
 
 class ApiConfig(AppConfig):
@@ -10,29 +9,4 @@ class ApiConfig(AppConfig):
     name = 'apps.api'
 
     def ready(self):
-        group = self.get_model("Group")
-        watson.register(
-            group,
-            fields=(
-                "name",
-            ),
-        )
-
-        # chorus = self.get_model("Chorus")
-        # watson.register(
-        #     chorus,
-        #     fields=(
-        #         "name",
-        #     ),
-        # )
-
-        # quartet = self.get_model("Quartet")
-        # watson.register(
-        #     quartet,
-        #     fields=(
-        #         "name",
-        #     )
-        # )
-
-        # convention = self.get_model("Convention")
-        # watson.register(convention)
+        pass
