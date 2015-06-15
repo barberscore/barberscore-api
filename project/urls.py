d671dd363f5e943b64eecec035a2f7d699929f32
David Binetti <dbinetti@gmail.com>
2015-06-15 11:24:31 -0700

    Remove browseable API

--- project/urls.py
+++ project/urls.py
@@ -6,10 +6,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    # url(r'^', include('noncense.urls', namespace='noncense')),
-    # url(r'^', include('apps.website.urls', namespace='website')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('apps.api.urls')),
     url(r'^auth/', include('djoser.urls')),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
