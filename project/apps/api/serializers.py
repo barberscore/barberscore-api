51f28f07795a990066a215318793ac2769420a0d
David Binetti <dbinetti@gmail.com>
2015-06-18 07:56:36 -0700

    Add BSMDB support

--- project/apps/api/serializers.py
+++ project/apps/api/serializers.py
@@ -122,6 +122,7 @@ class GroupSerializer(serializers.ModelSerializer):
             'tenor',
             'baritone',
             'bass',
+            'bsmdb',
             # 'contestants',
         )
 
