b3133e08a2acffe6b319504ca0d9df2d9524ac45
David Binetti <dbinetti@gmail.com>
2015-06-19 12:50:05 -0700

    Update admin for convention

--- project/apps/api/models.py
+++ project/apps/api/models.py
@@ -381,7 +381,7 @@ class Convention(models.Model):
         (MIDWINTER, 'Midwinter',),
         (FALL, 'Fall',),
         (SPRING, 'Spring',),
-        (PACIFIC, 'Pan-Pacific',),
+        (PACIFIC, 'Pacific',),
     )
 
     id = models.UUIDField(
@@ -471,68 +471,6 @@ class Convention(models.Model):
 
 
 class Contest(models.Model):
-
-    # BHS = 0
-    # CAR = 1
-    # CSD = 2
-    # DIX = 3
-    # EVG = 4
-    # FWD = 5
-    # ILL = 6
-    # JAD = 7
-    # LOL = 8
-    # MAD = 9
-    # NED = 10
-    # NSC = 11
-    # ONT = 12
-    # PIO = 13
-    # RMD = 14
-    # SLD = 15
-    # SUN = 16
-    # SWD = 17
-    # BABS = 18
-    # BHA = 19
-    # BHNZ = 20
-    # BING = 21
-    # DABS = 22
-    # FABS = 23
-    # IABS = 24
-    # NZABS = 25
-    # SABS = 26
-    # SNOBS = 27
-    # SPATS = 28
-
-    # DISTRICT_CHOICES = (
-    #     (BHS, "BHS"),
-    #     (CAR, "CAR"),
-    #     (CSD, "CSD"),
-    #     (DIX, "DIX"),
-    #     (EVG, "EVG"),
-    #     (FWD, "FWD"),
-    #     (ILL, "ILL"),
-    #     (JAD, "JAD"),
-    #     (LOL, "LOL"),
-    #     (MAD, "MAD"),
-    #     (NED, "NED"),
-    #     (NSC, "NSC"),
-    #     (ONT, "ONT"),
-    #     (PIO, "PIO"),
-    #     (RMD, "RMD"),
-    #     (SLD, "SLD"),
-    #     (SUN, "SUN"),
-    #     (SWD, "SWD"),
-    #     (BABS, "BABS"),
-    #     (BHA, "BHA"),
-    #     (BHNZ, "BHNZ"),
-    #     (BING, "BING"),
-    #     (DABS, "DABS"),
-    #     (FABS, "FABS"),
-    #     (IABS, "IABS"),
-    #     (NZABS, "NZABS"),
-    #     (SABS, "SABS"),
-    #     (SNOBS, "SNOBS"),
-    #     (SPATS, "SPATS"),
-    # )
 
     YEAR_CHOICES = []
     for r in range(1994, (datetime.datetime.now().year + 1)):
