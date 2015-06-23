369f5858d688cb51464add139b85176787aa0f4b
David Binetti <dbinetti@gmail.com>
2015-06-23 14:54:49 -0700

    Update denormalization command

--- project/apps/api/management/commands/denormalize.py
+++ project/apps/api/management/commands/denormalize.py
@@ -14,16 +14,16 @@ class Command(BaseCommand):
     help = "Command to denormailze data."
 
     def handle(self, *args, **options):
-        vs = Convention.objects.all()
-        for v in vs:
-            v.save()
-        ts = Contest.objects.all()
-        for t in ts:
-            t.save()
-        cs = Contestant.objects.all()
-        for c in cs:
-            c.save()
         ps = Performance.objects.all()
         for p in ps:
             p.save()
+        cs = Contestant.objects.all()
+        for c in cs:
+            c.save()
+        ts = Contest.objects.all()
+        for t in ts:
+            t.save()
+        vs = Convention.objects.all()
+        for v in vs:
+            v.save()
         return "Done"
