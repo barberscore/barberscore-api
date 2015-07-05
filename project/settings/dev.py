ba0ceb3ffb2cd4b73b461afd57c197aefbb48949
David Binetti <dbinetti@gmail.com>
2015-07-05 16:25:49 -0700

    Remove custom logging from dev

--- project/settings/dev.py
+++ project/settings/dev.py
@@ -23,41 +23,6 @@ NOSE_ARGS = [
     '--nologcapture',
 ]
 
-LOGGING = {
-    'version': 1,
-    "disable_existing_loggers": True,
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple'
-        },
-    },
-    'formatters': {
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'loggers': {
-        'apps.api': {
-            'level': 'DEBUG',
-            'handlers': ['console'],
-        },
-        'apps.website': {
-            'level': 'DEBUG',
-            'handlers': ['console'],
-        },
-        # 'noncense': {
-        #     'level': 'DEBUG',
-        #     'handlers': ['console'],
-        # },
-        'utils': {
-            'level': 'DEBUG',
-            'handlers': ['console'],
-        },
-    },
-}
-
 INSTALLED_APPS += (
     'debug_toolbar',
     'django_nose',
