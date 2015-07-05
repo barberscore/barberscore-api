93eaa779b715ddb68723994c1d095fdb93dcecbc
David Binetti <dbinetti@gmail.com>
2015-07-05 16:13:47 -0700

    Revert to default logging

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -39,11 +39,6 @@ STATICFILES_STORAGE = STATIC_STORAGE
 EMAIL_BACKEND = 'djrill.mail.backends.djrill.DjrillBackend'
 MANDRILL_API_KEY = get_env_variable("MANDRILL_APIKEY")
 
-BUGSNAG = {
-    "api_key": get_env_variable("BUGSNAG_API_KEY"),
-    "project_root": PROJECT_ROOT,
-}
-
 # Haystack
 from urlparse import urlparse
 es = urlparse(get_env_variable("SEARCHBOX_URL"))
@@ -57,55 +52,10 @@ HAYSTACK_CONNECTIONS = {
     },
 }
 
-MIDDLEWARE_CLASSES += (
-    "bugsnag.django.middleware.BugsnagMiddleware",
-)
-
 ALLOWED_HOSTS = [
     get_env_variable("HEROKU_HOST"),
     'api.barberscore.com',
 ]
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'root': {
-        'level': 'ERROR',
-        'handlers': ['bugsnag'],
-    },
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(message)s'
-        },
-    },
-    'loggers': {
-        'api': {
-            'level': 'DEBUG',
-            'handlers': ['console'],
-        },
-        # 'noncense': {
-        #     'level': 'DEBUG',
-        #     'handlers': ['console'],
-        # },
-        'utils': {
-            'level': 'DEBUG',
-            'handlers': ['console'],
-        },
-    },
-    'handlers': {
-        'bugsnag': {
-            'level': 'ERROR',
-            'class': 'bugsnag.handlers.BugsnagHandler',
-            'formatter': 'verbose',
-        },
-        'console': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose',
-            'stream': sys.stdout,
-        },
-    },
-}
 
 INSTALLED_APPS += (
     'djrill',
