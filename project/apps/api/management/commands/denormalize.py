fa86c244d65dadf5d5fd1cf533a0dd22865e0080
David Binetti <dbinetti@gmail.com>
2015-07-07 18:25:24 -0700

    Update denorm command

--- project/apps/api/management/commands/denormalize.py
+++ project/apps/api/management/commands/denormalize.py
@@ -6,7 +6,11 @@ from apps.api.models import (
     Convention,
     Contest,
     Contestant,
+    Performance,
     Group,
+    Person,
+    Singer,
+    Director,
 )
 
 
@@ -14,16 +18,28 @@ class Command(BaseCommand):
     help = "Command to denormailze data."
 
     def handle(self, *args, **options):
-        cs = Contestant.objects.all()
-        for c in cs:
-            c.save()
-        ts = Contest.objects.all()
-        for t in ts:
-            t.save()
         vs = Convention.objects.all()
         for v in vs:
             v.save()
+        ts = Contest.objects.all()
+        for t in ts:
+            t.save()
+        cs = Contestant.objects.all()
+        for c in cs:
+            c.save()
+        ps = Performance.objects.all()
+        for p in ps:
+            p.save()
         gs = Group.objects.all()
         for g in gs:
             g.save()
+        rs = Person.objects.all()
+        for r in rs:
+            r.save()
+        ss = Singer.objects.all()
+        for s in ss:
+            s.save()
+        ds = Director.objects.all()
+        for d in ds:
+            d.save()
         return "Done"
