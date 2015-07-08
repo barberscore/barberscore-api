5cf35dc7d694f04a19da61c6776535e6268e4da0
David Binetti <dbinetti@gmail.com>
2015-07-08 13:55:54 -0700

    Add default logging

--- project/settings/base.py
+++ project/settings/base.py
@@ -122,6 +122,30 @@ HAYSTACK_CONNECTIONS = {
     },
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
 # Applications
 INSTALLED_APPS = (
     'utils',
