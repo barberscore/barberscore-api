c99b1ffa8e21cffde6d50c08c3e67e191753d019
David Binetti <dbinetti@gmail.com>
2015-07-09 04:48:01 -0700

    Remove Djoser

--- project/urls.py
+++ project/urls.py
@@ -8,5 +8,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('apps.api.urls')),
-    url(r'^auth/', include('djoser.urls')),
+    # url(r'^auth/', include('djoser.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
