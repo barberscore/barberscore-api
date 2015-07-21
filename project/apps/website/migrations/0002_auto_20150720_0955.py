fdde13a9a3101f7a4fb2976b7375c046e8b27ec0
David Binetti <dbinetti@gmail.com>
2015-07-21 07:17:07 -0700

    Polished deduplicator

---
+++ project/apps/website/migrations/0002_auto_20150720_0955.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('website', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Primitive',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='collection',
+            name='primitive',
+            field=models.ForeignKey(related_name='collections', blank=True, to='website.Primitive', null=True),
+        ),
+    ]
