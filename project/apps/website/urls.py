8ff585f6e4ae04f549d8d4f4e6fe7df44309c454
David Binetti <dbinetti@gmail.com>
2015-07-26 00:40:26 -0700

    song merge tool

--- project/apps/website/urls.py
+++ project/apps/website/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     url(r'^quartet/$', views.quartets, name='quartets'),
     url(r'^song/$', views.songs, name='songs'),
     url(r'^merge-group/(?P<parent>[a-zA-Z0-9-]+)/(?P<child>[a-zA-Z0-9-]+)/$', views.merge_group, name='merge_group'),
+    url(r'^merge-song/(?P<parent>[a-zA-Z0-9-]+)/(?P<child>[a-zA-Z0-9-]+)/$', views.merge_song, name='merge_song'),
     url(r'^collection/(?P<id>[a-zA-Z0-9-]+)/$', views.collection, name='collection'),
     url(r'^merge/(?P<id>[a-zA-Z0-9-]+)/$', views.merge, name='merge'),
     url(r'^skip/(?P<id>[a-zA-Z0-9-]+)/$', views.skip, name='skip'),
