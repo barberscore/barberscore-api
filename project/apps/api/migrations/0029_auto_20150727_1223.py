3f2ee839858ad681349ba3df25801ea6571540f6
David Binetti <dbinetti@gmail.com>
2015-07-27 12:23:35 -0700

    Reverse and re-run migration

--- project/apps/api/migrations/0029_auto_20150727_1223.py
+++ project/apps/api/migrations/0029_auto_20150727_1223.py
@@ -2,19 +2,18 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import autoslug.fields
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0030_spot_slug'),
+        ('api', '0028_auto_20150727_1138'),
     ]
 
     operations = [
         migrations.AlterField(
             model_name='spot',
-            name='slug',
-            field=autoslug.fields.AutoSlugField(always_update=True, populate_from=b'name', editable=False),
+            name='name',
+            field=models.CharField(max_length=200),
         ),
     ]
