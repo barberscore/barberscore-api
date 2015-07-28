7e2d8ec95594d8f9a9b84a9fa924358334d3b92c
David Binetti <dbinetti@gmail.com>
2015-07-28 11:44:47 -0700

    Update duplication tool

--- project/apps/website/urls.py
+++ project/apps/website/urls.py
@@ -10,12 +10,16 @@ urlpatterns = [
     url(r'^chorus/$', views.choruses, name='choruses'),
     url(r'^quartet/$', views.quartets, name='quartets'),
     url(r'^song/$', views.songs, name='songs'),
-    url(r'^merge-groups/(?P<parent_id>[a-zA-Z0-9-]+)/(?P<child_id>[a-zA-Z0-9-]+)/$', views.merge_groups, name='merge_groups'),
-    url(r'^remove-group/(?P<parent_id>[a-zA-Z0-9-]+)/$', views.remove_group, name='remove_group'),
-    url(r'^merge-songs/(?P<parent_id>[a-zA-Z0-9-]+)/(?P<child_id>[a-zA-Z0-9-]+)/$', views.merge_songs, name='merge_songs'),
-    url(r'^remove-song/(?P<parent_id>[a-zA-Z0-9-]+)/$', views.remove_song, name='remove_song'),
-    url(r'^merge-persons/(?P<parent_id>[a-zA-Z0-9-]+)/(?P<child_id>[a-zA-Z0-9-]+)/$', views.merge_persons, name='merge_persons'),
-    url(r'^remove-person/(?P<parent_id>[a-zA-Z0-9-]+)/$', views.remove_person, name='remove_person'),
+    url(r'^build-chorus/$', views.build_chorus, name='build-chorus'),
+    url(r'^build-quartet/$', views.build_quartet, name='build-quartet'),
+    url(r'^build-song/$', views.build_song, name='build-song'),
+    url(r'^build-person/$', views.build_person, name='build-person'),
+    url(r'^merge-groups/(?P<parent_id>[a-zA-Z0-9-]+)/(?P<child_id>[a-zA-Z0-9-]+)/$', views.merge_groups, name='merge-groups'),
+    url(r'^remove-group/(?P<parent_id>[a-zA-Z0-9-]+)/$', views.remove_group, name='remove-group'),
+    url(r'^merge-songs/(?P<parent_id>[a-zA-Z0-9-]+)/(?P<child_id>[a-zA-Z0-9-]+)/$', views.merge_songs, name='merge-songs'),
+    url(r'^remove-song/(?P<parent_id>[a-zA-Z0-9-]+)/$', views.remove_song, name='remove-song'),
+    url(r'^merge-persons/(?P<parent_id>[a-zA-Z0-9-]+)/(?P<child_id>[a-zA-Z0-9-]+)/$', views.merge_persons, name='merge-persons'),
+    url(r'^remove-person/(?P<parent_id>[a-zA-Z0-9-]+)/$', views.remove_person, name='remove-person'),
 ]
 
 if settings.DEBUG:
