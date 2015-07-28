e322659da16cd5606ae9605caa2f16e5baba3879
David Binetti <dbinetti@gmail.com>
2015-07-28 10:31:39 -0700

    Update duplication management controllers

--- project/apps/website/urls.py
+++ project/apps/website/urls.py
@@ -10,12 +10,12 @@ urlpatterns = [
     url(r'^chorus/$', views.choruses, name='choruses'),
     url(r'^quartet/$', views.quartets, name='quartets'),
     url(r'^song/$', views.songs, name='songs'),
-    url(r'^merge-groups/(?P<parent>[a-zA-Z0-9-]+)/(?P<child>[a-zA-Z0-9-]+)/$', views.merge_groups, name='merge_groups'),
-    url(r'^remove-group/(?P<parent>[a-zA-Z0-9-]+)/$', views.remove_group, name='remove_group'),
-    url(r'^merge-songs/(?P<parent>[a-zA-Z0-9-]+)/(?P<child>[a-zA-Z0-9-]+)/$', views.merge_songs, name='merge_songs'),
-    url(r'^remove-song/(?P<parent>[a-zA-Z0-9-]+)/$', views.remove_song, name='remove_song'),
-    url(r'^merge-persons/(?P<parent>[a-zA-Z0-9-]+)/(?P<child>[a-zA-Z0-9-]+)/$', views.merge_persons, name='merge_persons'),
-    url(r'^remove-person/(?P<parent>[a-zA-Z0-9-]+)/$', views.remove_person, name='remove_person'),
+    url(r'^merge-groups/(?P<parent_id>[a-zA-Z0-9-]+)/(?P<child_id>[a-zA-Z0-9-]+)/$', views.merge_groups, name='merge_groups'),
+    url(r'^remove-group/(?P<parent_id>[a-zA-Z0-9-]+)/$', views.remove_group, name='remove_group'),
+    url(r'^merge-songs/(?P<parent_id>[a-zA-Z0-9-]+)/(?P<child_id>[a-zA-Z0-9-]+)/$', views.merge_songs, name='merge_songs'),
+    url(r'^remove-song/(?P<parent_id>[a-zA-Z0-9-]+)/$', views.remove_song, name='remove_song'),
+    url(r'^merge-persons/(?P<parent_id>[a-zA-Z0-9-]+)/(?P<child_id>[a-zA-Z0-9-]+)/$', views.merge_persons, name='merge_persons'),
+    url(r'^remove-person/(?P<parent_id>[a-zA-Z0-9-]+)/$', views.remove_person, name='remove_person'),
 ]
 
 if settings.DEBUG:
