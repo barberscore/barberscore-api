33a440bd6da9c32656d248fe234750228f887206
David Binetti <dbinetti@gmail.com>
2015-10-09 05:55:41 -0700

    Add Scores model

---
+++ project/apps/api/migrations/0004_auto_20151009_0536.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('api', '0003_score'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='chart',
+            name='songs',
+        ),
+        migrations.DeleteModel(
+            name='Chart',
+        ),
+    ]
