63b223287d8ac0a39b946297860b16a983d3050a
David Binetti <dbinetti@gmail.com>
2015-10-12 11:16:49 -0700

    TimeStamp the Common model

---
+++ project/apps/api/migrations/0019_auto_20151012_1116.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.utils.timezone
+import model_utils.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('api', '0018_auto_20151012_1113'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='district',
+            name='created',
+            field=model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, verbose_name='created', editable=False),
+        ),
+        migrations.AddField(
+            model_name='district',
+            name='modified',
+            field=model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, verbose_name='modified', editable=False),
+        ),
+        migrations.AddField(
+            model_name='group',
+            name='created',
+            field=model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, verbose_name='created', editable=False),
+        ),
+        migrations.AddField(
+            model_name='group',
+            name='modified',
+            field=model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, verbose_name='modified', editable=False),
+        ),
+        migrations.AddField(
+            model_name='person',
+            name='created',
+            field=model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, verbose_name='created', editable=False),
+        ),
+        migrations.AddField(
+            model_name='person',
+            name='modified',
+            field=model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, verbose_name='modified', editable=False),
+        ),
+    ]
