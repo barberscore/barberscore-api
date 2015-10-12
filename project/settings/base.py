e8645dc4a178d1fc8728537a6b30ce7b00cf3bd6
David Binetti <dbinetti@gmail.com>
2015-10-12 07:33:52 -0700

    Add default permissions

--- project/settings/base.py
+++ project/settings/base.py
@@ -96,6 +96,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ),
 }
 
 #  CORS Headers
