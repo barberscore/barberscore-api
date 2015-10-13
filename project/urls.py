c45a02b80769e032bcbdaa230e369104c86c4f24
David Binetti <dbinetti@gmail.com>
2015-10-13 09:45:28 -0700

    Add Score and Schedule

--- project/urls.py
+++ project/urls.py
@@ -8,6 +8,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('apps.api.urls')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # url(r'^docs/', include('rest_framework_swagger.urls')),
     url(r'^website/', include('apps.website.urls', namespace='website')),
     url(r'^robots.txt$', TemplateView.as_view(template_name='robots.txt')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
