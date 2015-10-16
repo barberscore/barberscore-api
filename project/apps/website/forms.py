7b38bf21dd853983663f7095613cfc6be3c66ee5
David Binetti <dbinetti@gmail.com>
2015-10-16 10:12:57 -0700

    Add custom User Model

--- project/apps/website/forms.py
+++ project/apps/website/forms.py
@@ -13,6 +13,46 @@ from django.db import (
 )
 
 
+class LoginForm(forms.Form):
+
+    email = forms.EmailField(
+        required=True,
+        # label=popover_html(
+        #     'Email',
+        #     'help-login-email',
+        #     'Please enter the email you used when you originally registered for the site.'
+        # ),
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': "Email",
+                'autofocus': 'autofocus',
+            },
+        ),
+        error_messages={
+            'required': 'Please enter your email address.',
+        },
+    )
+
+    password = forms.CharField(
+        required=True,
+        # label=popover_html(
+        #     'Password',
+        #     'help-login-password',
+        #     'Please enter the password you used at registration.  If you have forgotten your password please contact support.'
+        # ),
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': "Password",
+            },
+        ),
+        error_messages={
+            'required': 'Please enter your password.',
+        },
+    )
+
+
 class MergePersonForm(forms.Form):
     old = forms.CharField(
         max_length=200,
