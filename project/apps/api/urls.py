a4dc06812acefd4d96b064405756ef6b56530aaa
David Binetti <dbinetti@gmail.com>
2015-10-16 22:25:49 -0700

    Add Appearance model

--- project/apps/api/urls.py
+++ project/apps/api/urls.py
@@ -17,6 +17,7 @@ from .views import (
     JudgeViewSet,
     EventViewSet,
     AwardViewSet,
+    AppearanceViewSet,
 )
 
 router = routers.DefaultRouter()
@@ -36,5 +37,6 @@ router.register(r'score', ScoreViewSet)
 router.register(r'judge', JudgeViewSet)
 router.register(r'event', EventViewSet)
 router.register(r'award', AwardViewSet)
+router.register(r'appearance', AppearanceViewSet)
 router.register(r'search', SearchViewSet, base_name='search')
 urlpatterns = router.urls
