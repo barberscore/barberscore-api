4f4d5e84ed1dacf1388ee1488435950178612d43
David Binetti <dbinetti@gmail.com>
2015-10-21 08:39:29 -0700

    Remove smuggler

--- project/urls.py
+++ project/urls.py
@@ -6,7 +6,6 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    url(r'^admin/', include('smuggler.urls')),
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('apps.api.urls')),
