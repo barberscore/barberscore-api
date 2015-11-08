f455dbd2aa201eb7c034b86fb963c544933ca973
David Binetti <dbinetti@gmail.com>
2015-11-08 06:38:35 -0800

    Add status monitor

---
+++ project/apps/api/migrations/0101_judge_status_monitor.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.utils.timezone
+import model_utils.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('api', '0100_person_organization'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='judge',
+            name='status_monitor',
+            field=model_utils.fields.MonitorField(default=django.utils.timezone.now, help_text=b'Status last updated', monitor=b'status'),
+        ),
+    ]
