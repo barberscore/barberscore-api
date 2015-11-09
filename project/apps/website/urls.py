80357fc827c3d88d63a6db6d6f213a668c8a52fe
David Binetti <dbinetti@gmail.com>
2015-11-09 13:01:57 -0800

    Disable URLs; pre-FSM approach

--- project/apps/website/urls.py
+++ project/apps/website/urls.py
@@ -19,21 +19,18 @@ urlpatterns = [
     ),
 
     url(r'^dashboard/$', views.dashboard, name='dashboard'),
-    url(r'^contest/(?P<slug>[a-zA-Z0-9-]+)/build/$', views.contest_build, name='contest_build'),
-    url(r'^contest/(?P<slug>[a-zA-Z0-9-]+)/impanel/$', views.contest_impanel, name='contest_impanel'),
-    url(r'^contest/(?P<slug>[a-zA-Z0-9-]+)/contestants/$', views.contest_contestants, name='contest_contestants'),
-    url(r'^contest/(?P<slug>[a-zA-Z0-9-]+)/draw/$', views.contest_draw, name='contest_draw'),
-    url(r'^contest/(?P<slug>[a-zA-Z0-9-]+)/start/$', views.contest_start, name='contest_start'),
-    url(r'^contest/(?P<slug>[a-zA-Z0-9-]+)/score/$', views.contest_score, name='contest_score'),
-    url(r'^contest/(?P<slug>[a-zA-Z0-9-]+)/confirm/$', views.contest_confirm, name='contest_confirm'),
     url(r'^contest/(?P<slug>[a-zA-Z0-9-]+)/$', views.contest, name='contest'),
+    # url(r'^contest/(?P<slug>[a-zA-Z0-9-]+)/build/$', views.contest_build, name='contest_build'),
+    # url(r'^contest/(?P<slug>[a-zA-Z0-9-]+)/impanel/$', views.contest_impanel, name='contest_impanel'),
+    # url(r'^contest/(?P<slug>[a-zA-Z0-9-]+)/fill/$', views.contest_fill, name='contest_fill'),
+    # url(r'^contest/(?P<slug>[a-zA-Z0-9-]+)/start/$', views.contest_start, name='contest_start'),
+    # url(r'^session/(?P<slug>[a-zA-Z0-9-]+)/draw/$', views.session_draw, name='session_draw'),
+    # url(r'^session/(?P<slug>[a-zA-Z0-9-]+)/start/$', views.session_start, name='session_start'),
+    # url(r'^performance/(?P<slug>[a-zA-Z0-9-]+)/score/$', views.performance_score, name='performance_score'),
+    # url(r'^session/(?P<slug>[a-zA-Z0-9-]+)/end/$', views.session_end, name='session_end'),
+    # url(r'^contest/(?P<slug>[a-zA-Z0-9-]+)/end/$', views.contest_end, name='contest_end'),
 
-
-
-
-    url(r'^session/(?P<slug>[a-zA-Z0-9-]+)/$', views.session, name='session'),
-    url(r'^performance/(?P<slug>[a-zA-Z0-9-]+)/$', views.performance, name='performance'),
-
+    # Scoresheets
     url(r'^session/(?P<slug>[a-zA-Z0-9-]+)/oss/$', views.session_oss, name='session-oss'),
     url(r'^contest/(?P<slug>[a-zA-Z0-9-]+)/oss/$', views.contest_oss, name='contest-oss'),
     url(r"^contest/(?P<slug>[a-zA-Z0-9-]+)/pdf/$", views.HelloPDFView.as_view())
