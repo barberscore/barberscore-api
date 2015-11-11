7e25eca1aa5f5688ff4fd8978aad5df44484a7ae
David Binetti <dbinetti@gmail.com>
2015-11-11 11:33:37 -0800

    Update managers

--- project/apps/api/managers.py
+++ project/apps/api/managers.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import (
+    BaseUserManager,
+)
+
+from django.db import (
+    models,
+)
+
 # from django.db.models.query import (
 #     QuerySet,
 # )
@@ -18,3 +26,33 @@
 
 #     def contest(self):
 #         return self.filter(category__in=[0, 1, 2, 3])
+
+class SessionManager(models.Manager):
+    def initial(self):
+        return self.order_by('-kind').first()
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password, **kwargs):
+        user = self.model(
+            email=self.normalize_email(email),
+            is_active=True,
+            **kwargs
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password, **kwargs):
+        user = self.model(
+            email=email,
+            is_staff=True,
+            is_superuser=True,
+            is_active=True,
+            **kwargs
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
