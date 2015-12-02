99fb5ac2cce6da4aa6e0febf0a67f1da66aa41de
David Binetti <dbinetti@gmail.com>
2015-12-01 21:22:39 -0800

    Mini squash [ci skip]

--- project/apps/api/migrations/0158_auto_20151201_2122.py
+++ project/apps/api/migrations/0158_auto_20151201_2122.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
@@ -13,11 +14,16 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AlterModelOptions(
             name='panelist',
-            options={'ordering': ('panel', 'category', 'slot')},
+            options={},
+        ),
+        migrations.AlterField(
+            model_name='score',
+            name='panelist',
+            field=models.ForeignKey(related_name='scores', on_delete=django.db.models.deletion.SET_NULL, to='api.Panelist', null=True),
         ),
         migrations.AlterUniqueTogether(
             name='panelist',
-            unique_together=set([('panel', 'category', 'slot'), ('panel', 'person')]),
+            unique_together=set([]),
         ),
         migrations.RemoveField(
             model_name='panelist',
