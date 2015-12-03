a4bed2a1e9b5fed75590eb974eccba04b2c9257d
David Binetti <dbinetti@gmail.com>
2015-12-03 10:06:16 -0800

    Fix autocomplete

---
+++ project/apps/api/migrations/0022_auto_20151203_1005.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('api', '0021_auto_20151203_0959'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='contestant',
+            options={},
+        ),
+        migrations.AlterModelOptions(
+            name='panel',
+            options={},
+        ),
+        migrations.AlterModelOptions(
+            name='performance',
+            options={},
+        ),
+    ]
