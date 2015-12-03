47c77c74a670aea17019ef2d66a7ebf039d109e5
David Binetti <dbinetti@gmail.com>
2015-12-03 10:48:33 -0800

    Update models and denormalize

--- project/apps/api/management/commands/denormalize.py
+++ project/apps/api/management/commands/denormalize.py
@@ -8,10 +8,12 @@ from apps.api.models import (
     Contestant,
     Performance,
     Song,
-    Group,
-    Singer,
-    Director,
-    Panelist,
+    # Singer,
+    # Director,
+    # Panelist,
+    Ranking,
+    Session,
+    Panel,
 )
 
 
@@ -19,28 +21,37 @@ class Command(BaseCommand):
     help = "Command to denormailze data."
 
     def handle(self, *args, **options):
-        vs = Convention.objects.all()
-        for v in vs:
-            v.save()
-        ts = Contest.objects.all()
-        for t in ts:
-            t.save()
-        cs = Contestant.objects.all()
-        for c in cs:
-            c.save()
-        as_ = Performance.objects.all()
-        for a in as_:
-            a.save()
         ps = Song.objects.all()
         for p in ps:
             p.save()
-        ss = Singer.objects.all()
+        as_ = Performance.objects.all()
+        for a in as_:
+            a.save()
+        cs = Contestant.objects.all()
+        for c in cs:
+            c.save()
+        rs = Ranking.objects.all()
+        for r in rs:
+            r.save()
+        ss = Session.objects.all()
         for s in ss:
             s.save()
-        js = Panelist.objects.all()
-        for j in js:
-            j.save()
-        ds = Director.objects.all()
-        for d in ds:
-            d.save()
+        ts = Contest.objects.all()
+        for t in ts:
+            t.save()
+        ps = Panel.objects.all()
+        for p in ps:
+            p.save()
+        vs = Convention.objects.all()
+        for v in vs:
+            v.save()
+        # ss = Singer.objects.all()
+        # for s in ss:
+        #     s.save()
+        # js = Panelist.objects.all()
+        # for j in js:
+        #     j.save()
+        # ds = Director.objects.all()
+        # for d in ds:
+        #     d.save()
         return "Done"
