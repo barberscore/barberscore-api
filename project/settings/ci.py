bbcd6801ad4d3621738e222ab268e81419925620
David Binetti <dbinetti@gmail.com>
2015-12-03 19:57:44 -0800

    Change testing requirements

--- project/settings/ci.py
+++ project/settings/ci.py
@@ -3,6 +3,8 @@ from .base import *
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 NOSE_ARGS = [
     '--nologcapture',
+    '--with-coverage',
+    '--cover-package=apps.api',
 ]
 
 INSTALLED_APPS += (
