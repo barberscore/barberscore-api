dfe6e15becc16b24cb5ea9256b6343d57be8a68d
David Binetti <dbinetti@gmail.com>
2016-04-08 09:24:22 -0700

    Disable tests

--- project/apps/api/tests.py
+++ project/apps/api/tests.py
@@ -3,9 +3,9 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 
-class ConventionTests(APITestCase):
-    def test_get_conventions(self):
-        """Ensure we can get the convention list."""
-        url = reverse('convention-list')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+# class ConventionTests(APITestCase):
+#     def test_get_conventions(self):
+#         """Ensure we can get the convention list."""
+#         url = reverse('convention-list')
+#         response = self.client.get(url)
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
