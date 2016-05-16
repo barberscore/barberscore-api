31f79beaa561ad297cc81d29b9f014f2999ebd12
David Binetti <dbinetti@gmail.com>
2016-05-16 15:16:40 -0700

    Add Organization test

--- project/apps/api/tests.py
+++ project/apps/api/tests.py
@@ -4,15 +4,27 @@ from rest_framework.test import APITestCase
 
 from .factories import (
     ConventionFactory,
+    OrganizationFactory,
 )
 
 
 class ConventionTests(APITestCase):
     def setUp(self):
-        ConventionFactory()
+        ConventionFactory.create()
 
     def test_get_conventions(self):
         """Ensure we can get the convention list."""
         url = reverse('convention-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class OrganizationTests(APITestCase):
+    def setUp(self):
+        OrganizationFactory.create()
+
+    def test_get_organizations(self):
+        """Ensure we can get the organization list."""
+        url = reverse('organization-list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
