894f0176164e65763e9b84170ece730a767bc08d
David Binetti <dbinetti@gmail.com>
2016-06-28 10:34:30 -0700

    Try new S3 uploader

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -2,32 +2,36 @@
 from .base import *
 
 # AWS S3  Settings
-# This was hellaciously confusing to set up.
 # `Static` means public-read, static resources like CSS, Images, etc.
-# `Media` means private, user or admin-uploaded resources that have ACL
+# `Media` means user or admin-uploaded resources
+
+# Configure AWS variables
+AWS_ACCESS_KEY_ID = get_env_variable("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = get_env_variable("AWS_SECRET_ACCESS_KEY")
+AWS_PRELOAD_METADATA = True
+AWS_REGION = "us-west-1"
+AWS_S3_KEY_PREFIX = "files/"
+AWS_S3_KEY_PREFIX_STATIC = "static/"
+AWS_S3_BUCKET_AUTH = False
+AWS_S3_BUCKET_NAME = "{0}".format(PROJECT_NAME)
+AWS_S3_BUCKET_NAME_STATIC = "{0}".format(PROJECT_NAME)
+AWS_S3_MAX_AGE_SECONDS_STATIC = 60 * 60  # 1 hour
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# Configure AWS variables
-# Access credentials (global)
-AWS_ACCESS_KEY_ID = get_env_variable("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = get_env_variable("AWS_SECRET_ACCESS_KEY")
-AWS_PRELOAD_METADATA = True
-
 # Static Server Config
-AWS_STATIC_BUCKET_NAME = "{0}".format(PROJECT_NAME)
-STATIC_STORAGE = 'backends.StaticS3BotoStorage'
-STATIC_URL = 'https://{0}.s3-us-west-1.amazonaws.com/'.format(
-    AWS_STATIC_BUCKET_NAME,
-)
+STATIC_STORAGE = 'django_s3_storage.storage.StaticS3Storage'
+# STATIC_URL = 'https://{0}.s3-us-west-1.amazonaws.com/'.format(
+#     AWS_STATIC_BUCKET_NAME,
+# )
 
-# Media (aka File Upload) Server Config
+# Media (File Upload) Server Config
 AWS_MEDIA_BUCKET_NAME = "{0}".format(PROJECT_NAME)
-MEDIA_STORAGE = 'backends.MediaS3BotoStorage'
-MEDIA_URL = 'https://{0}.s3-us-west-1.amazonaws.com/'.format(
-    AWS_MEDIA_BUCKET_NAME,
-)
+MEDIA_STORAGE = 'django_s3_storage.storage.S3Storage'
+# MEDIA_URL = 'https://{0}.s3-us-west-1.amazonaws.com/'.format(
+#     AWS_MEDIA_BUCKET_NAME,
+# )
 
 # Aliasing Django Defaults
 DEFAULT_FILE_STORAGE = MEDIA_STORAGE
@@ -53,3 +57,9 @@ ALLOWED_HOSTS = [
     get_env_variable("HEROKU_HOST"),
     'api.barberscore.com',
 ]
+
+INSTALLED_APPS += (
+    'debug_toolbar',
+    'django_nose',
+    'django_s3_storage',
+)
