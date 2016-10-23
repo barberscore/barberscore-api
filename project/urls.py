b3362c05032b66592b8592ccb94a3ec3f10f815f
David Binetti <dbinetti@gmail.com>
2016-10-23 14:26:30 -0700

    Add media server to dev server

--- project/urls.py
+++ project/urls.py
@@ -5,10 +5,12 @@ from django.conf.urls import (
 )
 from django.contrib import admin
 from django.http import HttpResponse
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('apps.api.urls')),
     url(r'^api-auth/', include('rest_framework.urls')),
     url(r'^robots.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /", content_type="text/plain")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
