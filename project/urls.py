5294a2fc89785b94fb792881b0d88e135b240612
David Binetti <dbinetti@gmail.com>
2017-01-12 05:12:48 -0800

    Enable Debug Toolbar

--- project/urls.py
+++ project/urls.py
@@ -14,3 +14,9 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls')),
     url(r'^robots.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /", content_type="text/plain")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
