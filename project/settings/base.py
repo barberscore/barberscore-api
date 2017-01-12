3fead6459197773167b1e75a4ad3ffcba7e48f0c
David Binetti <dbinetti@gmail.com>
2017-01-12 05:25:11 -0800

    Convert tuples to lists

--- project/settings/base.py
+++ project/settings/base.py
@@ -101,29 +101,24 @@ PHONENUMBER_DEFAULT_FORMAT = 'NATIONAL'
 
 # Rest Framework (JSONAPI)
 REST_FRAMEWORK = {
-    # DJA settings
-    'PAGE_SIZE': 100,
+    'PAGE_SIZE': 10,
     'EXCEPTION_HANDLER': 'rest_framework_json_api.exceptions.exception_handler',
-    'DEFAULT_PARSER_CLASSES': (
+    'DEFAULT_METADATA_CLASS': 'rest_framework_json_api.metadata.JSONAPIMetadata',
+    'DEFAULT_PARSER_CLASSES': [
         'rest_framework_json_api.parsers.JSONParser',
         'rest_framework.parsers.FormParser',
         'rest_framework.parsers.MultiPartParser',
         'rest_framework.parsers.JSONParser'
-    ),
-    'DEFAULT_RENDERER_CLASSES': (
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
         'rest_framework_json_api.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
         'rest_framework.renderers.JSONRenderer',
-    ),
-    'DEFAULT_METADATA_CLASS': 'rest_framework_json_api.metadata.JSONAPIMetadata',
-    # And other DRF settings
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'dry_rest_permissions.generics.DRYPermissions',
     ],
 }
 
