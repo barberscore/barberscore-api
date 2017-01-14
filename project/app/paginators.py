1a3cfb612d61a4b6a685dd328fc05d98417401b6
David Binetti <dbinetti@gmail.com>
2017-01-14 09:57:00 -0800

    Update default pagination

--- project/app/paginators.py
+++ project/app/paginators.py
@@ -1,14 +1,14 @@
 from collections import OrderedDict
 from rest_framework.views import Response
 from rest_framework.pagination import (
-    PageNumberPagination,
-    LimitOffsetPagination,
+    PageNumberPagination as DRFPageNumberPagination,
+    LimitOffsetPagination as DRFLimitOffsetPagination,
     # CursorPagination,
 )
 from rest_framework.utils.urls import remove_query_param, replace_query_param
 
 
-class PageNumberPagination(PageNumberPagination):
+class PageNumberPagination(DRFPageNumberPagination):
     """
     A json-api compatible pagination format
     """
@@ -33,13 +33,12 @@ class PageNumberPagination(PageNumberPagination):
 
         return Response({
             'results': data,
-            'meta': {
-                'pagination': OrderedDict([
-                    ('page', self.page.number),
-                    ('pages', self.page.paginator.num_pages),
-                    ('count', self.page.paginator.count),
-                ])
-            },
+            'meta': OrderedDict([
+                ('per', self.page_size),
+                ('page', self.page.number),
+                ('pages', self.page.paginator.num_pages),
+                ('count', self.page.paginator.count),
+            ]),
             'links': OrderedDict([
                 ('first', self.build_link(1)),
                 ('last', self.build_link(self.page.paginator.num_pages)),
@@ -49,7 +48,7 @@ class PageNumberPagination(PageNumberPagination):
         })
 
 
-class LimitOffsetPagination(LimitOffsetPagination):
+class LimitOffsetPagination(DRFLimitOffsetPagination):
     """
     A limit/offset based style. For example:
     http://api.example.org/accounts/?page[limit]=100
@@ -83,41 +82,15 @@ class LimitOffsetPagination(LimitOffsetPagination):
     def get_paginated_response(self, data):
         return Response({
             'results': data,
-            'meta': {
-                'pagination': OrderedDict([
-                    ('count', self.count),
-                    ('limit', self.limit),
-                    ('offset', self.offset),
-                ])
-            },
+            'meta': OrderedDict([
+                ('page-size', self.page_size),
+                ('page', self.page.number),
+                ('total-pages', self.page.paginator.num_pages),
+            ]),
             'links': OrderedDict([
                 ('first', self.get_first_link()),
                 ('last', self.get_last_link()),
                 ('next', self.get_next_link()),
                 ('prev', self.get_previous_link())
             ])
-        })
-
-
-class PersonPaginator(PageNumberPagination):
-    def get_paginated_response(self, data):
-        next = None
-        previous = None
-
-        if self.page.has_next():
-            next = self.page.next_page_number()
-        if self.page.has_previous():
-            previous = self.page.previous_page_number()
-        return Response({
-            'results': data,
-            'meta': OrderedDict([
-                ('page', self.page.number),
-                ('total_pages', self.page.paginator.num_pages),
-            ]),
-            'links': OrderedDict([
-                ('first', self.build_link(1)),
-                ('last', self.build_link(self.page.paginator.num_pages)),
-                ('next', self.build_link(next)),
-                ('prev', self.build_link(previous))
-            ]),
         })
