c8b2ed7f61ce8ef0a84f8fde5dd3a6a8017ba8af
David Binetti <dbinetti@gmail.com>
2017-03-07 22:22:08 -0800

    Update settings for AUTH0

--- project/settings/base.py
+++ project/settings/base.py
@@ -126,23 +126,20 @@ JSON_API_FORMAT_KEYS = 'dasherize'
 APPEND_TRAILING_SLASH = False
 
 # Auth0 Settings:
-AUTH0_CLIENT_SECRET = get_env_variable("AUTH0_CLIENT_SECRET")
 AUTH0_CLIENT_ID = get_env_variable("AUTH0_CLIENT_ID")
+AUTH0_CLIENT_SECRET = get_env_variable("AUTH0_CLIENT_SECRET")
+AUTH0_API_ID = get_env_variable("AUTH0_API_ID")
+AUTH0_API_SECRET = get_env_variable("AUTH0_API_SECRET")
 AUTH0_DOMAIN = get_env_variable("AUTH0_DOMAIN")
 AUTH0_TOKEN = get_env_variable("AUTH0_TOKEN")
 
 
 # JWT Settings
-JWT_SECRET_KEY = base64.b64decode(
-    AUTH0_CLIENT_SECRET.replace("_", "/").replace("-", "+")
-)
-
-
 def jwt_get_username_from_payload_handler(payload):
     return payload.get('email')
 
 JWT_AUTH = {
-    'JWT_SECRET_KEY': JWT_SECRET_KEY,
+    'JWT_SECRET_KEY': AUTH0_CLIENT_SECRET,
     'JWT_AUDIENCE': AUTH0_CLIENT_ID,
     'JWT_PAYLOAD_GET_USERNAME_HANDLER': jwt_get_username_from_payload_handler,
     'JWT_AUTH_HEADER_PREFIX': 'Bearer',
