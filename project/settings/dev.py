49bcc7986fb14bc092507c46a2d071defa29f01c
David Binetti <dbinetti@gmail.com>
2017-04-27 09:53:15 -0700

    Update media URL settings

--- project/settings/dev.py
+++ project/settings/dev.py
@@ -24,13 +24,13 @@ INTERNAL_IPS = [
 # Static Server Config
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_STORAGE = STATIC_STORAGE
 
 # Media (aka File Upload) Server Config
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 MEDIA_STORAGE = 'django.core.files.storage.FileSystemStorage'
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 DEFAULT_FILE_STORAGE = MEDIA_STORAGE
 
 # CORS Settings
