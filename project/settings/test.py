cc614b7642d94afb5251a96d9a14349b651183b6
David Binetti <dbinetti@gmail.com>
2017-05-09 10:00:33 -0700

    Override services in test env.

--- project/settings/test.py
+++ project/settings/test.py
@@ -10,5 +10,11 @@ DATABASES['default']['TEST'] = {
     'NAME': DATABASES['default']['NAME'],
 }
 
+#  Docraptor
+DOCRAPTOR_API_KEY = None
+
+# Cloudinary
+CLOUDINARY_URL = None
+
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
