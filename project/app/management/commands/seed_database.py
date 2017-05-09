12c265287858d78df791121ac04e56dbdd07bf0c
David Binetti <dbinetti@gmail.com>
2017-05-09 14:13:42 -0700

    Settings test commit

--- project/app/management/commands/seed_database.py
+++ project/app/management/commands/seed_database.py
@@ -3,8 +3,8 @@ from django.apps import apps as api_apps
 from django.core.management.base import BaseCommand
 
 
-from app.factories import (
-    AdminFactory,
+from app.models import (
+    User,
 )
 
 
@@ -12,4 +12,7 @@ class Command(BaseCommand):
     help = "Command to seed database."
 
     def handle(self, *args, **options):
-        AdminFactory()
+        User.objects.create_superuser(
+            'admin@barberscore.com',
+            'password',
+        )
