3ede333e45541af075fb78bc38df0800ec4a4e10
David Binetti <dbinetti@gmail.com>
2017-05-15 10:39:11 -0700

    Linting

--- project/api/views.py
+++ project/api/views.py
@@ -11,14 +11,14 @@ from rest_framework import (
     viewsets,
 )
 from rest_framework.decorators import (
-    detail_route,
+    # detail_route,
     list_route,
-    parser_classes,
+    # parser_classes,
 )
-from rest_framework.parsers import (
-    FormParser,
-    MultiPartParser,
-)
+# from rest_framework.parsers import (
+#     FormParser,
+#     MultiPartParser,
+# )
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework_csv.renderers import CSVRenderer
