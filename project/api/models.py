a834d1f326601af7028d84dedaad00dde25f2b4a
David Binetti <dbinetti@gmail.com>
2017-05-24 09:38:22 -0700

    Add part to participants

--- project/api/models.py
+++ project/api/models.py
@@ -2454,6 +2454,20 @@ class Participant(TimeStampedModel):
         default=STATUS.new,
     )
 
+    PART = Choices(
+        (1, 'tenor', 'Tenor'),
+        (2, 'lead', 'Lead'),
+        (3, 'baritone', 'Baritone'),
+        (4, 'bass', 'Bass'),
+        (5, 'director', 'Director'),
+    )
+
+    part = models.IntegerField(
+        choices=PART,
+        null=True,
+        blank=True,
+    )
+
     # FKs
     entry = models.ForeignKey(
         'Entry',
