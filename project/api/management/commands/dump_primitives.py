fb80ff00ab84df431e0ad39015652e08633ea8b3
David Binetti <dbinetti@gmail.com>
2017-06-06 22:31:35 -0700

    Add `dump_primitives` command

---
+++ project/api/management/commands/dump_primitives.py
@@ -0,0 +1,38 @@
+# Django
+from django.core.management.base import BaseCommand
+import json
+from django.core.serializers import serialize
+from django.core.serializers.json import DjangoJSONEncoder
+from itertools import chain
+
+from api.models import (
+    Entity,
+    Award,
+    Office,
+)
+
+
+class Command(BaseCommand):
+    help = "Command to dump BHS primitives."
+
+    def handle(self, *args, **options):
+        eqs = Entity.objects.filter(
+            kind__lt=30,
+            status=Entity.STATUS.active,
+        ).order_by(
+            'kind',
+            'name',
+        )
+        aqs = Award.objects.filter(
+            status=Award.STATUS.active,
+        )
+        oqs = Office.objects.filter(
+            status=Office.STATUS.active,
+        )
+        data = list(chain(eqs, aqs, oqs))
+        json = serialize(
+            'json',
+            data,
+            cls=DjangoJSONEncoder,
+        )
+        self.stdout.write(json)
