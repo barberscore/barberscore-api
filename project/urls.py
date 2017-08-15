d4ae776a60e76b80afd45ef472c8434e2f710d9a
David Binetti <dbinetti@gmail.com>
2017-08-15 04:51:36 -0700

    decruft

--- project/urls.py
+++ project/urls.py
@@ -2,8 +2,6 @@
 # Third-Party
 from rest_framework.documentation import include_docs_urls
 from rest_framework.schemas import get_schema_view
-# from django.views.generic import TemplateView
-# from api.views import variance, ann
 from django.conf import settings
 from django.conf.urls import (
     include,
@@ -19,8 +17,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     url(r'^$', lambda r: HttpResponseRedirect('admin/')),
     url(r'^admin/', admin.site.urls),
-    # url(r'^variance/$', variance),
-    # url(r'^ann/$', ann),
     url(r'^api/', include('api.urls')),
     url(r'^api-auth/', include('rest_framework.urls')),
     url(r'^schema/', schema_view),
