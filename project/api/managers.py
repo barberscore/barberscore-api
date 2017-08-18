1f7c21280b7135f026f1ff807ffc50c97587f6fd
David Binetti <dbinetti@gmail.com>
2017-08-18 10:25:51 -0700

    Update manager for Person requirement

--- project/api/managers.py
+++ project/api/managers.py
@@ -4,10 +4,11 @@ from django.contrib.auth.models import BaseUserManager
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, email, password='', **kwargs):
+    def create_user(self, email, password='', person, **kwargs):
         user = self.model(
             email=email,
             password='',
+            person=person,
             is_active=True,
             **kwargs
         )
@@ -15,9 +16,10 @@ class UserManager(BaseUserManager):
 
         return user
 
-    def create_superuser(self, email, password, **kwargs):
+    def create_superuser(self, email, password, person, **kwargs):
         user = self.model(
             email=email,
+            person=person,
             is_staff=True,
             is_active=True,
             **kwargs
