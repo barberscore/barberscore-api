8b841a6e8d12bd4e9854c0668103c0ec657da9de
David Binetti <dbinetti@gmail.com>
2017-08-17 22:52:48 -0700

    Update management command

--- project/api/management/commands/sync_persons.py
+++ project/api/management/commands/sync_persons.py
@@ -8,7 +8,7 @@ from django.core.management.base import BaseCommand
 # First-Party
 from api.models import Person
 from bhs.models import Human
-from api.updaters import update_or_create_persons
+from api.updaters import update_or_create_person_from_human
 
 from email_validator import (
     validate_email,
@@ -29,4 +29,12 @@ class Command(BaseCommand):
     help = "Command to sync database with BHS ."
 
     def handle(self, *args, **options):
-        update_or_create_persons()
+        self.stdout.write("Updating users...")
+        hs = Human.objects.exclude(bhs_id=536190)
+        total = hs.count()
+        i = 0
+        for h in hs:
+            update_or_create_person_from_human(h)
+            self.stdout.write("{0}/{1}".format(i, total), ending='\r')
+            self.stdout.flush()
+            i += 1
--- project/api/updaters.py
+++ project/api/updaters.py
@@ -43,88 +43,85 @@ from bhs.models import (
 
 log = logging.getLogger('updater')
 
-def update_or_create_persons():
-    hs = Human.objects.exclude(bhs_id=536190)
-    for h in hs:
-        first_name = h.first_name.strip()
-        middle_name = h.middle_name.strip()
-        last_name = h.last_name.strip()
-        nick_name = h.nick_name.strip()
-        if not first_name:
-            first_name = None
-        if not middle_name:
-            middle_name = None
-        if not last_name:
-            last_name = None
-        if nick_name and (nick_name != first_name):
-            nick_name = "({0})".format(nick_name)
-        else:
-            nick_name = None
-        name = " ".join(
-            map(
-                (lambda x: encoding.smart_text(x)),
-                filter(
-                    None, [
-                        first_name,
-                        middle_name,
-                        last_name,
-                        nick_name,
-                    ]
-                )
+def update_or_create_person_from_human(h):
+    first_name = h.first_name.strip()
+    middle_name = h.middle_name.strip()
+    last_name = h.last_name.strip()
+    nick_name = h.nick_name.strip()
+    if not first_name:
+        first_name = None
+    if not middle_name:
+        middle_name = None
+    if not last_name:
+        last_name = None
+    if nick_name and (nick_name != first_name):
+        nick_name = "({0})".format(nick_name)
+    else:
+        nick_name = None
+    name = " ".join(
+        map(
+            (lambda x: encoding.smart_text(x)),
+            filter(
+                None, [
+                    first_name,
+                    middle_name,
+                    last_name,
+                    nick_name,
+                ]
             )
         )
-        try:
-            v = validate_email(h.username)
-            email = v["email"].lower()
-        except EmailNotValidError as e:
-            email = None
-        birth_date = h.birth_date
-        if h.phone:
-            phone = h.phone
-        else:
-            phone = ''
-        if h.cell_phone:
-            cell_phone = h.cell_phone
-        else:
-            cell_phone = ''
-        if h.work_phone:
-            work_phone = h.work_phone
-        else:
-            work_phone = ''
-        bhs_id = h.bhs_id
-        if h.sex == 'male':
-            gender = 10
-        elif h.sex == 'female':
-            gender = 20
-        else:
-            gender = None
-        if h.primary_voice_part == 'Tenor':
-            part = 1
-        elif h.primary_voice_part == 'Lead':
-            part = 2
-        elif h.primary_voice_part == 'Baritone':
-            part = 3
-        elif h.primary_voice_part == 'Bass':
-            part = 4
-        else:
-            part = None
-        defaults = {
-            'name': name,
-            'email': email,
-            'birth_date': birth_date,
-            'phone': phone,
-            'cell_phone': cell_phone,
-            'work_phone': work_phone,
-            'bhs_id': bhs_id,
-            'gender': gender,
-            'part': part,
-        }
-        try:
-            person, created = Person.objects.update_or_create(
-                bhs_pk=h.id,
-                defaults=defaults,
-            )
-            log.info((person, created))
-        except IntegrityError as e:
-            log.error((h, str(e)))
-            continue
+    )
+    try:
+        v = validate_email(h.username)
+        email = v["email"].lower()
+    except EmailNotValidError as e:
+        email = None
+    birth_date = h.birth_date
+    if h.phone:
+        phone = h.phone
+    else:
+        phone = ''
+    if h.cell_phone:
+        cell_phone = h.cell_phone
+    else:
+        cell_phone = ''
+    if h.work_phone:
+        work_phone = h.work_phone
+    else:
+        work_phone = ''
+    bhs_id = h.bhs_id
+    if h.sex == 'male':
+        gender = 10
+    elif h.sex == 'female':
+        gender = 20
+    else:
+        gender = None
+    if h.primary_voice_part == 'Tenor':
+        part = 1
+    elif h.primary_voice_part == 'Lead':
+        part = 2
+    elif h.primary_voice_part == 'Baritone':
+        part = 3
+    elif h.primary_voice_part == 'Bass':
+        part = 4
+    else:
+        part = None
+    defaults = {
+        'name': name,
+        'email': email,
+        'birth_date': birth_date,
+        'phone': phone,
+        'cell_phone': cell_phone,
+        'work_phone': work_phone,
+        'bhs_id': bhs_id,
+        'gender': gender,
+        'part': part,
+    }
+    try:
+        person, created = Person.objects.update_or_create(
+            bhs_pk=h.id,
+            defaults=defaults,
+        )
+        log.info((person, created))
+    except IntegrityError as e:
+        log.error((h, str(e)))
