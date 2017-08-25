2685eacd66d3f31524d86f99ccd6e92d76ed297b
David Binetti <dbinetti@gmail.com>
2017-08-25 13:13:15 -0700

    Mute signal

--- project/api/factories.py
+++ project/api/factories.py
@@ -11,7 +11,7 @@ from factory.django import (
 from factory.fuzzy import FuzzyInteger
 
 # Django
-from django.db.models.signals import post_save, pre_delete
+from django.db.models.signals import post_save
 
 # First-Party
 from api.models import (
@@ -257,6 +257,7 @@ class ParticipantFactory(DjangoModelFactory):
         model = Participant
 
 
+@mute_signals(post_save)
 class PersonFactory(DjangoModelFactory):
     name = Faker('name_male')
     status = Person.STATUS.active
@@ -355,7 +356,7 @@ class VenueFactory(DjangoModelFactory):
         model = Venue
 
 
-@mute_signals(post_save, pre_delete)
+@mute_signals(post_save)
 class UserFactory(DjangoModelFactory):
     email = Faker('email')
     password = PostGenerationMethodCall('set_password', 'password')
