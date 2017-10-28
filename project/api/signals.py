6efed48d280bb5bcf5209495b80e16b35ca13c39
David Binetti <dbinetti@gmail.com>
2017-10-28 09:49:59 -0700

    Only update auth0 for `is_active` Users

--- project/api/signals.py
+++ project/api/signals.py
@@ -21,53 +21,54 @@ from .utils import get_auth0_token
 def user_post_save(sender, instance=None, created=False, raw=False, **kwargs):
     """Create/Update Auth0 from User."""
     if not raw:
-        if created:
-            token = get_auth0_token()
-            auth0 = Auth0(
-                settings.AUTH0_DOMAIN,
-                token,
-            )
-            payload = {
-                "connection": "email",
-                "email": instance.email,
-                "email_verified": True,
-                "user_metadata": {
-                    "name": instance.name
-                },
-                "app_metadata": {
-                    "barberscore_id": str(instance.id),
+        if instance.is_active:
+            if created:
+                token = get_auth0_token()
+                auth0 = Auth0(
+                    settings.AUTH0_DOMAIN,
+                    token,
+                )
+                payload = {
+                    "connection": "email",
+                    "email": instance.email,
+                    "email_verified": True,
+                    "user_metadata": {
+                        "name": instance.name
+                    },
+                    "app_metadata": {
+                        "barberscore_id": str(instance.id),
+                    }
                 }
-            }
-            try:
-                response = auth0.users.create(payload)
-            except Auth0Error as e:
-                if 'The user already exists' in e.message:
-                    return
-                else:
+                try:
+                    response = auth0.users.create(payload)
+                except Auth0Error as e:
+                    if 'The user already exists' in e.message:
+                        return
+                    else:
+                        raise(e)
+                instance.auth0_id = response['user_id']
+                instance.save()
+            else:
+                token = get_auth0_token()
+                auth0 = Auth0(
+                    settings.AUTH0_DOMAIN,
+                    token,
+                )
+                payload = {
+                    "connection": "email",
+                    "email": instance.email,
+                    "email_verified": True,
+                    "user_metadata": {
+                        "name": instance.name
+                    },
+                    "app_metadata": {
+                        "barberscore_id": str(instance.id),
+                    }
+                }
+                try:
+                    response = auth0.users.update(instance.auth0_id, payload)
+                except Auth0Error as e:
                     raise(e)
-            instance.auth0_id = response['user_id']
-            instance.save()
-        else:
-            token = get_auth0_token()
-            auth0 = Auth0(
-                settings.AUTH0_DOMAIN,
-                token,
-            )
-            payload = {
-                "connection": "email",
-                "email": instance.email,
-                "email_verified": True,
-                "user_metadata": {
-                    "name": instance.name
-                },
-                "app_metadata": {
-                    "barberscore_id": str(instance.id),
-                }
-            }
-            try:
-                response = auth0.users.update(instance.auth0_id, payload)
-            except Auth0Error as e:
-                raise(e)
 
 
 @receiver(pre_delete, sender=User)
