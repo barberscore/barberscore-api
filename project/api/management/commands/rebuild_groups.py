2a08fd3a3bc2f88b452e0e59c8e12b1a6ab95ba5
David Binetti <dbinetti@gmail.com>
2017-11-13 11:52:22 -0800

    Add stdout feedback

--- project/api/management/commands/rebuild_groups.py
+++ project/api/management/commands/rebuild_groups.py
@@ -10,8 +10,14 @@ class Command(BaseCommand):
     help = "Command to rebuild group parents."
 
     def handle(self, *args, **options):
+        self.stdout.write("Updating groups...")
         gs = Group.objects.all()
+        i = 0
+        t = gs.count()
         for g in gs:
+            i += 1
+            self.stdout.write("{0}/{1}".format(i, t), ending='\r')
+            self.stdout.flush()
             organization = g.organization
             while organization:
                 if organization.kind == organization.KIND.international:
@@ -22,3 +28,5 @@ class Command(BaseCommand):
                     g.division = organization
                 organization = organization.parent
             g.save()
+        self.stdout.write("Updated {0} groups.".format(t))
+        return
