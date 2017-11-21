870d0e18c96fbeb7c544f267c1e5ffe4cefd1178
David Binetti <dbinetti@gmail.com>
2017-11-21 14:27:27 -0800

    Re-add RQ

--- project/settings/test.py
+++ project/settings/test.py
@@ -10,5 +10,15 @@ DATABASES['default']['TEST'] = {
     'NAME': DATABASES['default']['NAME'],
 }
 
+# Redis
+RQ_QUEUES = {
+    'default': {
+        'URL': get_env_variable("REDIS_URL"),
+        'DEFAULT_TIMEOUT': 360,
+        'ASYNC': False,
+    },
+}
+RQ_SHOW_ADMIN_LINK = True
+
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
