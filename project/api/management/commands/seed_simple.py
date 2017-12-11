49ba232cdc8df12ba57d1331d61cfd189ceebb8b
David Binetti <dbinetti@gmail.com>
2017-12-11 15:24:15 -0800

    Seed_simple through primitives

--- project/api/management/commands/seed_simple.py
+++ project/api/management/commands/seed_simple.py
@@ -9,6 +9,7 @@ from api.factories import (
     ContestFactory,
     ConventionFactory,
     CompetitorFactory,
+    EnrollmentFactory,
     EntryFactory,
     GrantorFactory,
     GroupFactory,
@@ -23,7 +24,6 @@ from api.factories import (
     RoundFactory,
     ScoreFactory,
     SessionFactory,
-    SlotFactory,
     SongFactory,
     UserFactory,
     VenueFactory,
@@ -37,6 +37,7 @@ from api.models import (
     Contestant,
     Convention,
     Competitor,
+    Enrollment,
     Entry,
     Grantor,
     Group,
@@ -51,7 +52,6 @@ from api.models import (
     Round,
     Score,
     Session,
-    Slot,
     Song,
     User,
     Venue,
@@ -59,86 +59,140 @@ from api.models import (
 
 
 class Command(BaseCommand):
-    help = "Command to seed convention."
+    help = "Command to sync database with BHS ."
 
     def handle(self, *args, **options):
-        # Create Admin
-        admin_user = UserFactory(
-            name='Admin Person',
-            email='test@barberscore.com',
-            password='password',
-            is_staff=True,
-            is_active=True,
-        )
+        # Create Persons
         admin_person = PersonFactory(
             first_name='Admin',
             last_name='Person',
-            email='test@barberscore.com',
-            status=Person.STATUS.active,
-            user=admin_user,
-        )
-        # Create Core Users
-        scjc_user = UserFactory(
-            name='SCJC Person',
-            email='scjs@barberscore.com',
-            is_active=True,
-        )
-        drcj_user = UserFactory(
-            name='DRCJ Person',
-            email='drcj@barberscore.com',
-            is_active=True,
-        )
-        ca_user = UserFactory(
-            name='CA Person',
-            email='ca@barberscore.com',
-            is_active=True,
-        )
-        quartet_user = UserFactory(
-            name='Quartet Person',
-            email='quartet@barberscore.com',
-            is_active=True,
-        )
-        chorus_user = UserFactory(
-            name='Chorus Person',
-            email='chorus@barberscore.com',
-            is_active=True,
+            email='admin@barberscore.com',
         )
         scjc_person = PersonFactory(
             first_name='SCJC',
             last_name='Person',
             email='scjc@barberscore.com',
-            status=Person.STATUS.active,
-            user=scjc_user,
         )
         drcj_person = PersonFactory(
             first_name='DRCJ',
             last_name='Person',
             email='drcj@barberscore.com',
-            status=Person.STATUS.active,
-            user=drcj_user,
         )
         ca_person = PersonFactory(
             first_name='CA',
             last_name='Person',
             email='ca@barberscore.com',
-            status=Person.STATUS.active,
-            user=ca_user,
         )
-        quartet_person = PersonFactory(
+        quartet_admin_person = PersonFactory(
             first_name='Quartet',
-            last_name='Person',
-            email='quartet@barberscore.com',
-            status=Person.STATUS.active,
-            user=quartet_user,
+            last_name='Admin',
+            email='quartet_admin@barberscore.com',
         )
-        chorus_person = PersonFactory(
+        chorus_admin_person = PersonFactory(
             first_name='Chorus',
-            last_name='Person',
-            email='chorus@barberscore.com',
-            status=Person.STATUS.active,
-            user=chorus_user,
+            last_name='Admin',
+            email='chorus_admin@barberscore.com',
         )
-        # Create International and Districts
+        music_judge_person = PersonFactory(
+            first_name='Music',
+            last_name='Judge',
+            email='music_judge@barberscore.com',
+        )
+        performance_judge_person = PersonFactory(
+            first_name='Performance',
+            last_name='Judge',
+            email='performance_judge@barberscore.com',
+        )
+        singing_judge_person = PersonFactory(
+            first_name='Singing',
+            last_name='Judge',
+            email='singing_judge@barberscore.com',
+        )
+        # Create Users
+        admin_user = UserFactory(
+            name='{0} {1}'.format(
+                admin_person.first_name,
+                admin_person.last_name,
+            ),
+            email=admin_person.email,
+            password='password',
+            is_staff=True,
+            is_active=True,
+            auth0_id='email|5a18277f7cd31262971cddfc',
+        )
+        scjc_user = UserFactory(
+            name='{0} {1}'.format(
+                scjc_person.first_name,
+                scjc_person.last_name,
+            ),
+            email=scjc_person.email,
+            is_active=True,
+            auth0_id='email|5a1827707cd31262971cddf8',
+        )
+        drcj_user = UserFactory(
+            name='{0} {1}'.format(
+                drcj_person.first_name,
+                drcj_person.last_name,
+            ),
+            email=drcj_person.email,
+            is_active=True,
+            auth0_id='email|5a12f3607cd31262971bca22',
+        )
+        ca_user = UserFactory(
+            name='{0} {1}'.format(
+                ca_person.first_name,
+                ca_person.last_name,
+            ),
+            email=ca_person.email,
+            is_active=True,
+            auth0_id='email|5a1827777cd31262971cddf9',
+        )
+        quartet_admin_user = UserFactory(
+            name='{0} {1}'.format(
+                quartet_admin_person.first_name,
+                quartet_admin_person.last_name,
+            ),
+            email=quartet_admin_person.email,
+            is_active=True,
+            auth0_id='email|5a12f3627cd31262971bca24',
+        )
+        chorus_admin_user = UserFactory(
+            name='{0} {1}'.format(
+                chorus_admin_person.first_name,
+                chorus_admin_person.last_name,
+            ),
+            email=chorus_admin_person.email,
+            is_active=True,
+            auth0_id='email|5a12f3637cd31262971bca25',
+        )
+        music_judge_user = UserFactory(
+            name='{0} {1}'.format(
+                music_judge_person.first_name,
+                music_judge_person.last_name,
+            ),
+            email=music_judge_person.email,
+            is_active=True,
+            auth0_id='email|5a2ef1717cd312629752490b',
+        )
+        performance_judge_user = UserFactory(
+            name='{0} {1}'.format(
+                performance_judge_person.first_name,
+                performance_judge_person.last_name,
+            ),
+            email=performance_judge_person.email,
+            is_active=True,
+            auth0_id='email|5a2ef1717cd312629752490a',
+        )
+        singing_judge_user = UserFactory(
+            name='{0} {1}'.format(
+                singing_judge_person.first_name,
+                singing_judge_person.last_name,
+            ),
+            email=singing_judge_person.email,
+            is_active=True,
+            auth0_id='email|5a2ef1707cd3126297524909',
+        )
+        # Create Organizations
         international = OrganizationFactory(
             name='International Organization',
             code='INT',
@@ -156,7 +210,42 @@ class Command(BaseCommand):
             parent=district_alpha,
             kind=Organization.KIND.chapter,
         )
-        # Create Core Offices
+        # create Venue
+        venue = VenueFactory()
+
+        # create Chart
+        chart_one = ChartFactory(
+            title='Chart One',
+        )
+        chart_two = ChartFactory(
+            title='Chart Two',
+        )
+        chart_three = ChartFactory(
+            title='Chart Three',
+        )
+        chart_four = ChartFactory(
+            title='Chart Four',
+        )
+        chart_five = ChartFactory(
+            title='Chart Five',
+        )
+        chart_six = ChartFactory(
+            title='Chart Six',
+        )
+        chart_seven = ChartFactory(
+            title='Chart Seven',
+        )
+        chart_eight = ChartFactory(
+            title='Chart Eight',
+        )
+        chart_nine = ChartFactory(
+            title='Chart Nine',
+        )
+        chart_ten = ChartFactory(
+            title='Chart Ten',
+        )
+
+        # Create Offices
         scjc_office = OfficeFactory(
             name='Society Chairman of C&J',
             short_name='SCJC',
@@ -199,6 +288,68 @@ class Command(BaseCommand):
             short_name='SNG',
             is_judge_manager=True,
         )
+        # Create Groups
+        quartet_one = GroupFactory(
+            name='Quartet One',
+            organization=district_alpha,
+        )
+        # Create chorus
+        chorus_one = GroupFactory(
+            name='Chorus One',
+            organization=chapter_one,
+            kind=Group.KIND.chorus,
+        )
+        # Create enrollments
+        enrollment_chorus_admin = EnrollmentFactory(
+            organization=chapter_one,
+            person=chorus_admin_person,
+        )
+        # Create members
+        member_quartet_admin = MemberFactory(
+            part=Member.PART.lead,
+            is_admin=True,
+            group=quartet_one,
+            person=quartet_admin_person,
+        )
+        member_chorus_admin = MemberFactory(
+            part=Member.PART.lead,
+            is_admin=True,
+            group=chorus_one,
+            person=chorus_admin_person,
+        )
+        # Create repertories
+        RepertoryFactory(
+            group=quartet_one,
+            chart=chart_one,
+        )
+        RepertoryFactory(
+            group=quartet_one,
+            chart=chart_two,
+        )
+        RepertoryFactory(
+            group=quartet_one,
+            chart=chart_three,
+        )
+        RepertoryFactory(
+            group=quartet_one,
+            chart=chart_four,
+        )
+        RepertoryFactory(
+            group=quartet_one,
+            chart=chart_five,
+        )
+        RepertoryFactory(
+            group=quartet_one,
+            chart=chart_six,
+        )
+        RepertoryFactory(
+            group=chorus_one,
+            chart=chart_one,
+        )
+        RepertoryFactory(
+            group=chorus_one,
+            chart=chart_two,
+        )
         # Create Core Officers
         scjc_officer = OfficerFactory(
             office=scjc_office,
@@ -218,21 +369,21 @@ class Command(BaseCommand):
             organization=international,
             status=Officer.STATUS.active,
         )
-        mus_judges = OfficerFactory.create_batch(
-            size=5,
+        mus_judge = OfficerFactory(
             office=mus_office,
+            person=music_judge_person,
             organization=international,
             status=Officer.STATUS.active,
         )
-        per_judges = OfficerFactory.create_batch(
-            size=5,
+        per_judge = OfficerFactory(
             office=per_office,
+            person=performance_judge_person,
             organization=international,
             status=Officer.STATUS.active,
         )
-        sng_judges = OfficerFactory.create_batch(
-            size=5,
+        sng_judge = OfficerFactory(
             office=sng_office,
+            person=singing_judge_person,
             organization=international,
             status=Officer.STATUS.active,
         )
@@ -261,6 +412,7 @@ class Command(BaseCommand):
             kind=Award.KIND.chorus,
         )
 
+        return
         # Create conventions
         district_alpha_fall_convention = ConventionFactory(
             name='District Alpha Fall Convention',
@@ -274,6 +426,32 @@ class Command(BaseCommand):
             organization=district_alpha,
             convention=district_alpha_fall_convention,
         )
+        # Create assignments
+        AssignmentFactory(
+            category=Assignment.CATEGORY.drcj,
+            convention=district_alpha_fall_convention,
+            person=drcj_person,
+        )
+        AssignmentFactory(
+            category=Assignment.CATEGORY.ca,
+            convention=district_alpha_fall_convention,
+            person=ca_person,
+        )
+        AssignmentFactory(
+            category=Assignment.CATEGORY.music,
+            convention=district_alpha_fall_convention,
+            person=music_judge_person,
+        )
+        AssignmentFactory(
+            category=Assignment.CATEGORY.performance,
+            convention=district_alpha_fall_convention,
+            person=performance_judge_person,
+        )
+        AssignmentFactory(
+            category=Assignment.CATEGORY.singing,
+            convention=district_alpha_fall_convention,
+            person=singing_judge_person,
+        )
         district_alpha_fall_convention_quartet_session = SessionFactory(
             convention=district_alpha_fall_convention,
             kind=Session.KIND.quartet,
@@ -284,26 +462,4 @@ class Command(BaseCommand):
             kind=Session.KIND.chorus,
             num_rounds=1,
         )
-        # Create quartet
-        quartet_one = GroupFactory(
-            name='Quartet One',
-            organization=district_alpha,
-        )
-        # Overwrite user
-        member = quartet_one.members.first()
-        member.person = quartet_person
-        member.is_admin = True
-        member.save()
-
-        # Create chorus
-        chorus_one = GroupFactory(
-            name='Chorus One',
-            organization=chapter_one,
-            kind=Group.KIND.chorus,
-        )
-        # Overwrite user
-        member = chorus_one.members.first()
-        member.person = chorus_person
-        member.is_admin = True
-        member.save()
 
