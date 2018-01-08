21f392be55359c089ba3ef081b9e868bab604c2c
David Binetti <dbinetti@gmail.com>
2018-01-08 11:54:38 -0800

    Revert to SMTP

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -54,15 +54,15 @@ JWT_AUTH = {
 }
 
 # Email
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.sendgrid.net'
-# EMAIL_HOST_USER = get_env_variable("SENDGRID_USERNAME")
-# EMAIL_HOST_PASSWORD = get_env_variable("SENDGRID_PASSWORD")
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-EMAIL_SUBJECT_PREFIX = "[Barberscore] "
-EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
-SENDGRID_API_KEY = get_env_variable("SENDGRID_API_KEY")
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = get_env_variable("SENDGRID_USERNAME")
+EMAIL_HOST_PASSWORD = get_env_variable("SENDGRID_PASSWORD")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+# EMAIL_SUBJECT_PREFIX = "[Barberscore] "
+# EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
+# SENDGRID_API_KEY = get_env_variable("SENDGRID_API_KEY")
 
 # Cloudinary
 CLOUDINARY_URL = get_env_variable("CLOUDINARY_URL")
