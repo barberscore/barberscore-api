0cf2cf531903c0117f07e8a0313bdbbf49a57f3c
David Binetti <dbinetti@gmail.com>
2018-02-02 03:05:37 -0800

    Update sync persons and humans command

---
+++ project/api/management/commands/sync_persons_humans.py
@@ -0,0 +1,40 @@
+
+# Django
+from django.core.management.base import (
+    BaseCommand,
+)
+
+from api.models import (
+    Person,
+)
+
+from bhs.models import (
+    Human
+)
+from api.tasks import (
+    update_or_create_person_from_human,
+)
+
+
+class Command(BaseCommand):
+    help = "Command to sync persons and humans."
+
+    def handle(self, *args, **options):
+        self.stdout.write("Updating persons and humans...")
+        # Build list of humans
+        humans = Human.objects.all()
+        human_pks = list(humans.values_list('id', flat=True))
+        # Delete Orphans
+        self.stdout.write("Deleting orphans...")
+        orphans = Person.objects.filter(
+            bhs_pk__isnull=False,
+        ).exclude(
+            bhs_pk__in=human_pks,
+        )
+        for orphan in orphans:
+            orphan.delete()
+        # Creating/Update Persons
+        self.stdout.write("Queuing person updates...")
+        for human in humans:
+            update_or_create_person_from_human.delay(human)
+        self.stdout.write("Complete")
