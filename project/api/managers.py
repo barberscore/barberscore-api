ee2d6733674c9efb24d36c63f01c75688c2fce9d
David Binetti <dbinetti@gmail.com>
2018-02-08 10:58:28 -0800

    Try, try again.

--- project/api/managers.py
+++ project/api/managers.py
@@ -461,7 +461,10 @@ class OrganizationManager(Manager):
             email = ""
         phone = structure.phone.strip()
         # And the chapter code
-        code = getattr(structure, 'chapter_code', '')
+        if structure.chapter_code:
+            code = structure.chapter_code
+        else:
+            code = ''
         bhs_id = structure.bhs_id
         mem_clean = structure.status.name.replace("-", "_")
         mem_status = getattr(self.model.MEM_STATUS, mem_clean)
