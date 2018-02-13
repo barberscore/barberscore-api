82b4ea673aefd73384eb442c1769211d55c74c14
David Binetti <dbinetti@gmail.com>
2018-02-13 11:50:30 -0800

    Update test infrastructure

--- project/settings/test.py
+++ project/settings/test.py
@@ -11,3 +11,8 @@ RQ_QUEUES['default']['ASYNC'] = False
 
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Cloudinary
+CLOUDINARY_URL = None
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
