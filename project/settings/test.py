f56b87b51aee743298a95c0ef85d611be49e0910
David Binetti <dbinetti@gmail.com>
2018-02-13 12:09:38 -0800

    Update test settings

--- project/settings/test.py
+++ project/settings/test.py
@@ -11,8 +11,3 @@ RQ_QUEUES['default']['ASYNC'] = False
 
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-# Cloudinary
-CLOUDINARY_URL = None
-MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
