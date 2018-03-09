46893d6ae95b13683144473e6831b8c73bb6e8d1
David Binetti <dbinetti@gmail.com>
2018-03-08 21:05:35 -0800

    Align update_bhs script

--- project/api/management/commands/update_bhs.py
+++ project/api/management/commands/update_bhs.py
@@ -8,10 +8,9 @@ from django.utils import timezone
 
 # First-Party
 from api.models import Person
-from api.models import Member
-from api.models import Officer
 from bhs.models import Human
 from bhs.models import Structure
+from bhs.models import Subscription
 from bhs.models import Role
 from bhs.models import Join
 
@@ -60,35 +59,32 @@ class Command(BaseCommand):
         else:
             cursor = None
 
-        if cursor:
-            active_only = True
-        else:
-            active_only = False
-
         # Sync Persons
-        t = Human.objects.update_persons(cursor=cursor, active_only=active_only)
+        t = Human.objects.update_persons(cursor=cursor)
         self.stdout.write("Queued {0} persons.".format(t))
         t = Human.objects.delete_orphans()
         self.stdout.write("Deleted {0} person orphans.".format(t))
 
         # Sync Groups
-        t = Structure.objects.update_groups(cursor=cursor, active_only=active_only)
+        t = Structure.objects.update_groups(cursor=cursor)
         self.stdout.write("Queued {0} groups.".format(t))
         t = Structure.objects.delete_orphans()
         self.stdout.write("Deleted {0} group orphans.".format(t))
 
+        # Sync Subscriptions
+        t = Subscription.objects.update_persons(cursor=cursor)
+        self.stdout.write("Queued {0} person statuses.".format(t))
+
         # Sync Members
-        latest = Member.objects.latest('created').created
-        t = Join.objects.update_members(cursor=latest)
+        t = Join.objects.update_members(cursor=cursor)
         self.stdout.write("Queued {0} members.".format(t))
 
         # Sync Roles
-        latest = Officer.objects.latest('created').created
-        t = Role.objects.update_officers(cursor=latest)
+        t = Role.objects.update_officers(cursor=cursor)
         self.stdout.write("Queued {0} officers.".format(t))
 
         # Sync Users
-        t = Person.objects.update_users(cursor=cursor, active_only=active_only)
+        t = Person.objects.update_users(cursor=cursor)
         self.stdout.write("Queued {0} users.".format(t))
 
         self.stdout.write("Complete.")
