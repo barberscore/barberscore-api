2f001edf559f2a088de4a90c4b0458b2d4d7005e
David Binetti <dbinetti@gmail.com>
2018-03-10 14:26:36 -0800

    Apps config ready()

--- project/api/config.py
+++ project/api/config.py
@@ -9,13 +9,12 @@ class ApiConfig(AppConfig):
     name = 'api'
 
     def ready(self):
-        pass
-        # if os.environ['DJANGO_SETTINGS_MODULE'] != 'settings_ci':
-        #     from .signals import (
-        #         user_pre_delete,
-        #     )
-        #     import algoliasearch_django as algoliasearch
-        #     from .indexes import ChartIndex
-        #     Chart = self.get_model('chart')
-        #     algoliasearch.register(Chart, ChartIndex)
-        # return
+        if os.environ['DJANGO_SETTINGS_MODULE'] != 'settings.base':
+            from .signals import (
+                user_pre_delete,
+            )
+            import algoliasearch_django as algoliasearch
+            from .indexes import ChartIndex
+            Chart = self.get_model('chart')
+            algoliasearch.register(Chart, ChartIndex)
+        return
