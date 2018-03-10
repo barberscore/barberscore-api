5625a3b00a610b3e451cbcdcda6e64b943551529
David Binetti <dbinetti@gmail.com>
2018-03-10 15:40:53 -0800

    Change path for settings

--- project/wsgi.py
+++ project/wsgi.py
@@ -13,6 +13,6 @@ import os
 # Django
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.base")
 
 application = get_wsgi_application()
