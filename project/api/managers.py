152e1d4f9794a972c625ee59eaa5d282909bec0b
David Binetti <dbinetti@gmail.com>
2018-03-31 22:16:06 -0700

    Fix bad reference

--- project/api/managers.py
+++ project/api/managers.py
@@ -740,7 +740,7 @@ class PersonManager(Manager):
             )
         except self.model.DoesNotExist:
             Human = apps.get_model('bhs.human')
-            human = Human.objects.get(id=person)
+            human = Human.objects.get(id=human)
             person = self.update_or_create_from_human(human)
 
         # Instantiate prior values dictionary
