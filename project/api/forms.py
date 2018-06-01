38dd14f49c938ece92c704b91aed3a7bc3ce21cb
David Binetti <dbinetti@gmail.com>
2018-06-01 15:07:55 -0700

    Update user creation form

--- project/api/forms.py
+++ project/api/forms.py
@@ -14,15 +14,7 @@ class UserCreationForm(forms.ModelForm):
 
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.email = self.cleaned_data['person'].email.lower()
-        if self.cleaned_data['bhs_id']:
-            name = "{0} [{1}]".format(
-                self.cleaned_data['name'],
-                self.cleaned_data['bhs_id'],
-            )
-        else:
-            name = self.cleaned_data['name']
-        user.name = name
+        user.username = self.cleaned_data['username']
         user.set_unusable_password()
         if commit:
             user.save()
