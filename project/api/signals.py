0f114a144268bb611ff00db9917756a8c02f84b9
David Binetti <dbinetti@gmail.com>
2018-06-07 08:51:32 -0700

    Connect person to user account

--- project/api/signals.py
+++ project/api/signals.py
@@ -7,12 +7,17 @@ from django.conf import settings
 
 # Local
 from .models import User
+from .tasks import activate_user
 from .tasks import delete_account
 
 
 @receiver(pre_delete, sender=User)
 def user_pre_delete(sender, instance, **kwargs):
-    if settings.DJANGO_SETTINGS_MODULE == 'settings.prod':
+    allowed = any([
+        settings.DJANGO_SETTINGS_MODULE == 'settings.prod',
+        settings.DJANGO_SETTINGS_MODULE == 'settings.dev',
+    ])
+    if allowed:
         if not instance.is_staff:
             delete_account(instance)
     return
@@ -20,7 +25,11 @@ def user_pre_delete(sender, instance, **kwargs):
 
 @receiver(pre_save, sender=User)
 def user_pre_save(sender, instance, **kwargs):
-    if settings.DJANGO_SETTINGS_MODULE == 'settings.prod':
+    allowed = any([
+        settings.DJANGO_SETTINGS_MODULE == 'settings.prod',
+        settings.DJANGO_SETTINGS_MODULE == 'settings.dev',
+    ])
+    if allowed:
         if not instance.is_staff and not instance.person:
             activate_user(instance)
     return
