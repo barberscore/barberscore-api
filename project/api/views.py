fca91ade2361c35cc46186a0a3fd7ffd70800c1e
David Binetti <dbinetti@gmail.com>
2018-06-20 13:35:48 -0700

    Add verify action

--- project/api/views.py
+++ project/api/views.py
@@ -127,6 +127,14 @@ class AppearanceViewSet(viewsets.ModelViewSet):
         return Response(serializer.data)
 
     @action(methods=['post'], detail=True)
+    def verify(self, request, pk=None, **kwargs):
+        object = self.get_object()
+        object.verify(by=self.request.user)
+        object.save()
+        serializer = self.get_serializer(object)
+        return Response(serializer.data)
+
+    @action(methods=['post'], detail=True)
     def confirm(self, request, pk=None, **kwargs):
         object = self.get_object()
         object.confirm(by=self.request.user)
