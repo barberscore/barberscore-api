2e320c2c5bdc6aa36f3b8f5205cc5033237f143f
David Binetti <dbinetti@gmail.com>
2018-07-14 13:23:09 -0700

    Add `mock_round` command

---
+++ project/api/management/commands/mock_round.py
@@ -0,0 +1,55 @@
+# Django
+from django.apps import apps
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+import datetime
+from random import randint
+
+from api.models import Appearance
+from api.models import Round
+
+class Command(BaseCommand):
+    help = "Command to mock remaining round appearances."
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'round_id',
+            nargs='?',
+        )
+
+    def handle(self, *args, **options):
+        round = Round.objects.get(id=options['round_id'])
+        appearances = round.appearances.exclude(
+            status=Appearance.STATUS.verified,
+        )
+        for appearance in appearances:
+            prelim = appearance.competitor.entry.prelim
+            if not prelim:
+                prelim = randint(65, 80)
+            if prelim:
+                songs = appearance.songs.all()
+                for song in songs:
+                    scores = song.scores.all()
+                    for score in scores:
+                        d = randint(-4,4)
+                        score.points = prelim + d
+                        score.save()
+            if appearance.status == appearance.STATUS.new:
+                raise RuntimeError("Out of state")
+            if appearance.status == appearance.STATUS.built:
+                appearance.start()
+                appearance.finish()
+                appearance.verify()
+                appearance.save()
+                continue
+            if appearance.status == appearance.STATUS.started:
+                appearance.finish()
+                appearance.verify()
+                appearance.save()
+                continue
+            if appearance.status == appearance.STATUS.finished:
+                appearance.verify()
+                appearance.save()
+                continue
+        self.stdout.write("Mocked round")
+        return
