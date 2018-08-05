50110004824416f14ec57e461540f9fbf50136fc
David Binetti <dbinetti@gmail.com>
2018-08-04 21:10:00 -0700

    Fix tests

--- project/tests/test_api_as_admin.py
+++ project/tests/test_api_as_admin.py
@@ -16,14 +16,14 @@ def test_api_endpoint(admin_api_client, django_assert_num_queries):
 
 
 def test_appearance_endpoint_list(admin_api_client, appearance, django_assert_num_queries):
-    with django_assert_num_queries(3):
+    with django_assert_num_queries(5):
         path = reverse('appearance-list')
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
 
 def test_assignment_endpoint_list(admin_api_client, assignment, django_assert_num_queries):
-    with django_assert_num_queries(2):
+    with django_assert_num_queries(4):
         path = reverse('assignment-list')
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
@@ -37,42 +37,42 @@ def test_award_endpoint_list(admin_api_client, award, django_assert_num_queries)
 
 
 def test_chart_endpoint_list(admin_api_client, chart, django_assert_num_queries):
-    with django_assert_num_queries(4):
+    with django_assert_num_queries(6):
         path = reverse('chart-list')
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
 
 def test_competitor_endpoint_list(admin_api_client, competitor, django_assert_num_queries):
-    with django_assert_num_queries(3):
+    with django_assert_num_queries(5):
         path = reverse('competitor-list')
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
 
 def test_contest_endpoint_list(admin_api_client, contest, django_assert_num_queries):
-    with django_assert_num_queries(3):
+    with django_assert_num_queries(5):
         path = reverse('contest-list')
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
 
 def test_contestant_endpoint_list(admin_api_client, contestant, django_assert_num_queries):
-    with django_assert_num_queries(2):
+    with django_assert_num_queries(4):
         path = reverse('contestant-list')
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
 
 def test_convention_endpoint_list(admin_api_client, convention, django_assert_num_queries):
-    with django_assert_num_queries(5):
+    with django_assert_num_queries(7):
         path = reverse('convention-list')
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
 
 def test_entry_endpoint_list(admin_api_client, entry, django_assert_num_queries):
-    with django_assert_num_queries(3):
+    with django_assert_num_queries(5):
         path = reverse('entry-list')
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
@@ -93,14 +93,14 @@ def test_grid_endpoint_list(admin_api_client, grid, django_assert_num_queries):
 
 
 def test_group_endpoint_list(admin_api_client, group, django_assert_num_queries):
-    with django_assert_num_queries(10):
+    with django_assert_num_queries(12):
         path = reverse('group-list')
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
 
 def test_member_endpoint_list(admin_api_client, member, django_assert_num_queries):
-    with django_assert_num_queries(2):
+    with django_assert_num_queries(4):
         path = reverse('member-list')
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
@@ -114,35 +114,35 @@ def test_office_endpoint_list(admin_api_client, office, django_assert_num_querie
 
 
 def test_officer_endpoint_list(admin_api_client, officer, django_assert_num_queries):
-    with django_assert_num_queries(2):
+    with django_assert_num_queries(4):
         path = reverse('officer-list')
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
 
 def test_panelist_endpoint_list(admin_api_client, panelist, django_assert_num_queries):
-    with django_assert_num_queries(3):
+    with django_assert_num_queries(5):
         path = reverse('panelist-list')
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
 
 def test_person_endpoint_list(admin_api_client, person, django_assert_num_queries):
-    with django_assert_num_queries(6):
+    with django_assert_num_queries(8):
         path = reverse('person-list')
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
 
 def test_repertory_endpoint_list(admin_api_client, repertory, django_assert_num_queries):
-    with django_assert_num_queries(2):
+    with django_assert_num_queries(4):
         path = reverse('repertory-list')
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
 
 def test_round_endpoint_list(admin_api_client, round, django_assert_num_queries):
-    with django_assert_num_queries(5):
+    with django_assert_num_queries(7):
         path = reverse('round-list')
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
@@ -156,7 +156,7 @@ def test_score_endpoint_list(admin_api_client, score, django_assert_num_queries)
 
 
 def test_session_endpoint_list(admin_api_client, session, django_assert_num_queries):
-    with django_assert_num_queries(6):
+    with django_assert_num_queries(8):
         path = reverse('session-list')
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
@@ -170,14 +170,14 @@ def test_song_endpoint_list(admin_api_client, song, django_assert_num_queries):
 
 
 def test_venue_endpoint_list(admin_api_client, venue, django_assert_num_queries):
-    with django_assert_num_queries(4):
+    with django_assert_num_queries(6):
         path = reverse('venue-list')
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
 
 def test_user_endpoint_list(admin_api_client, user, django_assert_num_queries):
-    with django_assert_num_queries(2):
+    with django_assert_num_queries(4):
         path = reverse('user-list')
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
@@ -186,14 +186,14 @@ def test_user_endpoint_list(admin_api_client, user, django_assert_num_queries):
 # Detail Views
 
 def test_appearance_endpoint_detail(admin_api_client, appearance, django_assert_num_queries):
-    with django_assert_num_queries(2):
+    with django_assert_num_queries(3):
         path = reverse('appearance-detail', args=(str(appearance.id),))
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
 
 def test_assignment_endpoint_detail(admin_api_client, assignment, django_assert_num_queries):
-    with django_assert_num_queries(1):
+    with django_assert_num_queries(2):
         path = reverse('assignment-detail', args=(str(assignment.id),))
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
@@ -207,42 +207,42 @@ def test_award_endpoint_detail(admin_api_client, award, django_assert_num_querie
 
 
 def test_chart_endpoint_detail(admin_api_client, chart, django_assert_num_queries):
-    with django_assert_num_queries(3):
+    with django_assert_num_queries(4):
         path = reverse('chart-detail', args=(str(chart.id),))
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
 
 def test_competitor_endpoint_detail(admin_api_client, competitor, django_assert_num_queries):
-    with django_assert_num_queries(2):
+    with django_assert_num_queries(3):
         path = reverse('competitor-detail', args=(str(competitor.id),))
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
 
 def test_contest_endpoint_detail(admin_api_client, contest, django_assert_num_queries):
-    with django_assert_num_queries(2):
+    with django_assert_num_queries(3):
         path = reverse('contest-detail', args=(str(contest.id),))
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
 
 def test_contestant_endpoint_detail(admin_api_client, contestant, django_assert_num_queries):
-    with django_assert_num_queries(1):
+    with django_assert_num_queries(2):
         path = reverse('contestant-detail', args=(str(contestant.id),))
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
 
 def test_convention_endpoint_detail(admin_api_client, convention, django_assert_num_queries):
-    with django_assert_num_queries(4):
+    with django_assert_num_queries(5):
         path = reverse('convention-detail', args=(str(convention.id),))
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
 
 def test_entry_endpoint_detail(admin_api_client, entry, django_assert_num_queries):
-    with django_assert_num_queries(2):
+    with django_assert_num_queries(3):
         path = reverse('entry-detail', args=(str(entry.id),))
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
@@ -263,14 +263,14 @@ def test_grid_endpoint_detail(admin_api_client, grid, django_assert_num_queries)
 
 
 def test_group_endpoint_detail(admin_api_client, group, django_assert_num_queries):
-    with django_assert_num_queries(9):
+    with django_assert_num_queries(10):
         path = reverse('group-detail', args=(str(group.id),))
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
 
 def test_member_endpoint_detail(admin_api_client, member, django_assert_num_queries):
-    with django_assert_num_queries(1):
+    with django_assert_num_queries(2):
         path = reverse('member-detail', args=(str(member.id),))
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
@@ -284,35 +284,35 @@ def test_office_endpoint_detail(admin_api_client, office, django_assert_num_quer
 
 
 def test_officer_endpoint_detail(admin_api_client, officer, django_assert_num_queries):
-    with django_assert_num_queries(1):
+    with django_assert_num_queries(2):
         path = reverse('officer-detail', args=(str(officer.id),))
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
 
 def test_panelist_endpoint_detail(admin_api_client, panelist, django_assert_num_queries):
-    with django_assert_num_queries(2):
+    with django_assert_num_queries(3):
         path = reverse('panelist-detail', args=(str(panelist.id),))
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
 
 def test_person_endpoint_detail(admin_api_client, person, django_assert_num_queries):
-    with django_assert_num_queries(5):
+    with django_assert_num_queries(6):
         path = reverse('person-detail', args=(str(person.id),))
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
 
 def test_repertory_endpoint_detail(admin_api_client, repertory, django_assert_num_queries):
-    with django_assert_num_queries(1):
+    with django_assert_num_queries(2):
         path = reverse('repertory-detail', args=(str(repertory.id),))
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
 
 def test_round_endpoint_detail(admin_api_client, round, django_assert_num_queries):
-    with django_assert_num_queries(4):
+    with django_assert_num_queries(5):
         path = reverse('round-detail', args=(str(round.id),))
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
@@ -326,7 +326,7 @@ def test_score_endpoint_detail(admin_api_client, score, django_assert_num_querie
 
 
 def test_session_endpoint_detail(admin_api_client, session, django_assert_num_queries):
-    with django_assert_num_queries(5):
+    with django_assert_num_queries(6):
         path = reverse('session-detail', args=(str(session.id),))
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
@@ -340,14 +340,14 @@ def test_song_endpoint_detail(admin_api_client, song, django_assert_num_queries)
 
 
 def test_venue_endpoint_detail(admin_api_client, venue, django_assert_num_queries):
-    with django_assert_num_queries(3):
+    with django_assert_num_queries(4):
         path = reverse('venue-detail', args=(str(venue.id),))
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
 
 def test_user_endpoint_detail(admin_api_client, user, django_assert_num_queries):
-    with django_assert_num_queries(1):
+    with django_assert_num_queries(2):
         path = reverse('user-detail', args=(str(user.id),))
         response = admin_api_client.get(path)
         assert response.status_code == status.HTTP_200_OK
