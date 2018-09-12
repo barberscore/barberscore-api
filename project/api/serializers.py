39cd6cb0ab87f0566d4efee2836bf4e5a3baadc7
David Binetti <dbinetti@gmail.com>
2018-09-12 10:15:49 -0700

    Add is_primary contest to serializer

--- project/api/serializers.py
+++ project/api/serializers.py
@@ -200,6 +200,7 @@ class ContestSerializer(serializers.ModelSerializer):
             'id',
             'url',
             'status',
+            'is_primary',
             'num',
             'group',
             'session',
