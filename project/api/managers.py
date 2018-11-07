dc5da3cc74a556924f72307d614510a4147fcf91
David Binetti <dbinetti@gmail.com>
2018-11-07 09:36:11 -0800

    Refer to "current_through" for end_date

--- project/api/managers.py
+++ project/api/managers.py
@@ -381,7 +381,7 @@ class MemberManager(Manager):
         structure = str(join.structure.id)
         human = str(join.subscription.human.id)
         start_date = join.established_date
-        end_date = join.inactive_date
+        end_date = join.subscription.current_through
         part = join.vocal_part
 
         # Transform
