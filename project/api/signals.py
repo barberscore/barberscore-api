9ff029d92c9a8ca3d7d4054ddec96fc059a75c61
David Binetti <dbinetti@gmail.com>
2018-11-16 18:14:45 -0800

    Updated signal

--- project/api/signals.py
+++ project/api/signals.py
@@ -10,8 +10,8 @@ import django_rq
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
-from django.db.models.signals import post_save
 from django.db.models.signals import pre_delete
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 # Local
@@ -19,23 +19,18 @@ from .models import Person
 from .models import User
 
 
-# @receiver(post_save, sender=User)
-def user_post_save(sender, instance, created, **kwargs):
-    if not instance.is_staff:
-        if created:
-            account, new = instance.update_or_create_account()
-            if new:
-                # Set username if new; otherwise it's an overwrite and skip save
-                instance.username = account['user_id']
-                instance.save()
-        else:
-            queue = django_rq.get_queue('low')
-            queue.enqueue(
-                instance.update_or_create_account
-            )
+
+@receiver(post_save, sender=Person)
+def person_post_save(sender, instance, **kwargs):
+    if instance.user and instance.tracker.has_changed('email'):
+        queue = django_rq.get_queue('low')
+        queue.enqueue(
+            instance.user.update_or_create_account()
+        )
     return
 
-# @receiver(pre_delete, sender=User)
+
+@receiver(pre_delete, sender=User)
 def user_pre_delete(sender, instance, **kwargs):
     if not instance.is_staff:
         queue = django_rq.get_queue('low')
