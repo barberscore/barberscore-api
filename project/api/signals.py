2547215661e3c212392efeeec41cd05af46a1fdd
David Binetti <dbinetti@gmail.com>
2019-03-06 14:30:41 -0800

    post delete handler

--- project/api/signals.py
+++ project/api/signals.py
@@ -1,8 +1,8 @@
 import django_rq
 
 # Django
-from django.db.models.signals import pre_delete
 from django.db.models.signals import post_save
+from django.db.models.signals import post_delete
 from django.dispatch import receiver
 
 # Local
@@ -10,6 +10,7 @@ from .models import Person
 from .models import User
 
 from .tasks import person_post_save_handler
+from .tasks import user_post_delete_handler
 
 
 @receiver(post_save, sender=Person)
@@ -22,11 +23,11 @@ def person_post_save(sender, instance, created, **kwargs):
     return
 
 
-@receiver(pre_delete, sender=User)
-def user_pre_delete(sender, instance, **kwargs):
-    if not instance.is_staff:
-        queue = django_rq.get_queue('low')
-        queue.enqueue(
-            instance.delete_account
-        )
+@receiver(post_delete, sender=User)
+def user_post_delete(sender, instance, **kwargs):
+    queue = django_rq.get_queue('low')
+    queue.enqueue(
+        user_post_delete_handler,
+        instance,
+    )
     return
