60545f9f55c027919422d71c2e158abcef51cea9
David Binetti <dbinetti@gmail.com>
2019-05-06 18:45:10 -0700

    Decouple (#516)
    * Add Person in first decouple step
    
    * Decruft admin
    
    * Add managers.py
    
    * WIP
    
    * Update routers for models
    
    * Add Group, Member, Officer models
    
    * Import managers
    
    * Activate Admin
    
    * Update managers

--- project/routers.py
+++ project/routers.py
@@ -3,24 +3,54 @@ class BHSRouter(object):
 
     def db_for_read(self, model, **hints):
         """Attempt to read bhs models go to bhs_db."""
-        if model._meta.app_label == 'bhs':
+        models = [
+            'person',
+            'group',
+            'member',
+            'officer',
+        ]
+        if model._meta.app_label == 'bhs' and model._meta.model_name in models:
+            return 'default'
+        elif model._meta.app_label == 'bhs':
             return 'bhs_db'
         return None
 
     def db_for_write(self, model, **hints):
         """Attempt to write bhs models go to bhs_db."""
+        models = [
+            'person',
+            'group',
+            'member',
+            'officer',
+        ]
+        if model._meta.app_label == 'bhs' and model._meta.model_name in models:
+            return 'default'
         if model._meta.app_label == 'bhs':
             return 'bhs_db'
         return None
 
     def allow_relation(self, obj1, obj2, **hints):
         """Allow relations if a model in the bhs app is involved."""
-        # if obj1._meta.app_label == 'bhs' or obj2._meta.app_label == 'bhs':
-        #     return True
+        models = [
+            'person',
+            'group',
+            'member',
+            'officer',
+        ]
+        if (obj1._meta.app_label == 'bhs' and obj1._meta.model_name in models) or (obj2._meta.app_label == 'bhs' and obj1._meta.model_name in models):
+            return True
         return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         """Make sure the bhs app only appears in the 'bhs_db' database."""
+        models = [
+            'person',
+            'group',
+            'member',
+            'officer',
+        ]
+        if app_label == 'bhs' and model_name in models:
+            return True
         if app_label == 'bhs':
             return False
         return None
