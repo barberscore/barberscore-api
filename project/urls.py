a4584ecc22301772857fa59823481fc7013a3bd3
David Binetti <dbinetti@gmail.com>
2019-05-07 21:35:22 -0700

    Update URL wiring

--- project/urls.py
+++ project/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('', lambda r: HttpResponseRedirect('admin/')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    # path('bhs/', include('bhs.urls')),
+    path('stage/', include('stage.urls')),
     path('rq/', include('django_rq.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('schema/', schema_view),
