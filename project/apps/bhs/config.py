4a7551c9751678b30dff9744fa5d598fb580acbe
David Binetti <dbinetti@gmail.com>
2019-05-14 10:55:10 -0700

    Re-enable Algolia indexes

--- project/apps/bhs/config.py
+++ project/apps/bhs/config.py
@@ -9,14 +9,13 @@ class BhsConfig(AppConfig):
 
     def ready(self):
         import algoliasearch_django as algoliasearch
-        import api.signals
         from .indexes import ChartIndex
         from .indexes import PersonIndex
         from .indexes import GroupIndex
         Chart = self.get_model('chart')
         Person = self.get_model('person')
         Group = self.get_model('group')
-        # algoliasearch.register(Chart, ChartIndex)
-        # algoliasearch.register(Person, PersonIndex)
-        # algoliasearch.register(Group, GroupIndex)
+        algoliasearch.register(Chart, ChartIndex)
+        algoliasearch.register(Person, PersonIndex)
+        algoliasearch.register(Group, GroupIndex)
         return
