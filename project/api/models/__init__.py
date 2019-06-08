74af1d96e86e1a5ba877c2096ff682aa2b0eeb34
David Binetti <dbinetti@gmail.com>
2019-06-08 10:48:18 -0700

    Remove model references
    Still need to manually remove old models after this operation is complete

--- project/api/models/__init__.py
+++ project/api/models/__init__.py
@@ -1,13 +1,1 @@
-
-from .appearance import Appearance
-# from .contest import Contest
-from .contender import Contender
-# from .contestant import Contestant
-# from .entry import Entry
-from .outcome import Outcome
-from .panelist import Panelist
-from .round import Round
-from .score import Score
-# from .session import Session
-from .song import Song
 from .user import User
