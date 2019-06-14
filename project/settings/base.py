d2929f1ed7bd82ea46a04ddaff385e39dd914716
David Binetti <dbinetti@gmail.com>
2019-06-14 07:01:46 -0700

    Unthrottle

--- project/settings/base.py
+++ project/settings/base.py
@@ -217,14 +217,14 @@ REST_FRAMEWORK = {
         'api.renderers.NoGroupMembersJSONRenderer',
     ),
     'TEST_REQUEST_DEFAULT_FORMAT': 'vnd.api+json',
-    'DEFAULT_THROTTLE_CLASSES': (
-        'rest_framework.throttling.AnonRateThrottle',
-        'rest_framework.throttling.UserRateThrottle'
-    ),
-    'DEFAULT_THROTTLE_RATES': {
-        'anon': '100/day',
-        'user': '1000/day'
-    }
+    # 'DEFAULT_THROTTLE_CLASSES': (
+    #     'rest_framework.throttling.AnonRateThrottle',
+    #     'rest_framework.throttling.UserRateThrottle'
+    # ),
+    # 'DEFAULT_THROTTLE_RATES': {
+    #     'anon': '100/day',
+    #     'user': '1000/day'
+    # }
 }
 
 # JSONAPI
