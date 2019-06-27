1fb3eb64e0ca35e3dc7a63a93345693f0697d5a6
David Binetti <dbinetti@gmail.com>
2019-06-27 09:39:45 -0700

    Wire in signals to Round manager

--- project/apps/rmanager/config.py
+++ project/apps/rmanager/config.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class RmanagerConfig(AppConfig):
     name = 'apps.rmanager'
     verbose_name = 'Round Manager'
+
+    def ready(self):
+        from apps.rmanager import signals
+        return
