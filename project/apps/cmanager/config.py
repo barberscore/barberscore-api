aa55b90203e5f6ad41350a885852f4df2fe9c0a6
David Binetti <dbinetti@gmail.com>
2019-07-10 14:59:14 -0700

    Disable algolia

--- project/apps/cmanager/config.py
+++ project/apps/cmanager/config.py
@@ -7,11 +7,11 @@ class CmanagerConfig(AppConfig):
 
     def ready(self):
         from apps.cmanager import signals
-        import algoliasearch_django as algoliasearch
-        from .indexes import AwardIndex
-        from .indexes import ConventionIndex
-        Award = self.get_model('award')
-        Convention = self.get_model('convention')
-        algoliasearch.register(Award, AwardIndex)
-        algoliasearch.register(Convention, ConventionIndex)
+        # import algoliasearch_django as algoliasearch
+        # from .indexes import AwardIndex
+        # from .indexes import ConventionIndex
+        # Award = self.get_model('award')
+        # Convention = self.get_model('convention')
+        # algoliasearch.register(Award, AwardIndex)
+        # algoliasearch.register(Convention, ConventionIndex)
         return
