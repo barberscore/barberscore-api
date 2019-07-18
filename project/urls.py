77f59f6d376af5534988971dcd19c2e637cba315
David Binetti <dbinetti@gmail.com>
2019-07-18 12:14:20 -0700

    Cleanup and bump

--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,3 @@
-#
-
-
-# Third-Party
-from rest_framework.documentation import include_docs_urls
-from rest_framework.schemas import get_schema_view
-
 # Django
 from django.conf import settings
 from django.contrib import admin
@@ -14,10 +7,6 @@ from django.urls import include
 from django.urls import path
 from django.urls import re_path
 from django.views.static import serve
-
-schema_view = get_schema_view(
-    title='Barberscore API',
-)
 
 urlpatterns = [
     path('', lambda r: HttpResponseRedirect('admin/')),
@@ -29,9 +18,6 @@ urlpatterns = [
     path('jwt/', include('rest_framework_jwt.urls')),
     path('log/', include('django_fsm_log.urls')),
     path('rq/', include('django_rq.urls')),
-    path('api-auth/', include('rest_framework.urls')),
-    path('schema/', schema_view),
-    path('docs/', include_docs_urls(title='Barberscore Documentation')),
     path('robots.txt', lambda r: HttpResponse("User-agent: *\nDisallow: /", content_type="text/plain")),
 ]
 
