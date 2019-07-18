236f1b06012224ab025ad6bd0ebfe01d49e205da
David Binetti <dbinetti@gmail.com>
2019-07-17 22:07:52 -0700

    Disable BHS routers

--- project/settings/staging.py
+++ project/settings/staging.py
@@ -16,13 +16,13 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 SECURE_SSL_REDIRECT = True
 
 # Database
-DATABASES['bhs_db'] = dj_database_url.parse(
-    get_env_variable("BHS_DATABASE_URL"),
-    conn_max_age=600,
-)
-DATABASE_ROUTERS = [
-    'routers.BHSRouter',
-]
+# DATABASES['bhs_db'] = dj_database_url.parse(
+#     get_env_variable("BHS_DATABASE_URL"),
+#     conn_max_age=600,
+# )
+# DATABASE_ROUTERS = [
+#     'routers.BHSRouter',
+# ]
 
 # Sentry
 sentry_sdk.init(
