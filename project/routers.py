11dcac6fd275b3e913551a7293d9448d8b90e642
David Binetti <dbinetti@gmail.com>
2019-07-22 13:47:19 -0700

    Update routers.py

--- project/routers.py
+++ project/routers.py
@@ -2,63 +2,61 @@ class BHSRouter(object):
     """A router to control all database operations on models in the bhs application."""
 
     def db_for_read(self, model, **hints):
-        """Attempt to read bhs models go to bhs_db."""
+        """Attempt to read bhs models go to bhs_db, otherwise to default."""
         models = [
-            'person',
-            'group',
-            'member',
-            'officer',
-            'chart',
-            'repertory',
+            'bhs.human',
+            'bhs.structure',
+            'bhs.status',
+            'bhs.membership',
+            'bhs.subscription',
+            'bhs.role',
+            'bhs.join',
         ]
-        if model._meta.app_label == 'bhs' and model._meta.model_name in models:
-            return 'default'
-        elif model._meta.app_label == 'bhs':
+        if model._meta.label_lower in models:
             return 'bhs_db'
         return None
 
     def db_for_write(self, model, **hints):
-        """Attempt to write bhs models go to bhs_db."""
+        """Attempt to write bhs models go to bhs_db, otherwise to default."""
         models = [
-            'person',
-            'group',
-            'member',
-            'officer',
-            'chart',
-            'repertory',
+            'bhs.human',
+            'bhs.structure',
+            'bhs.status',
+            'bhs.membership',
+            'bhs.subscription',
+            'bhs.role',
+            'bhs.join',
         ]
-        if model._meta.app_label == 'bhs' and model._meta.model_name in models:
-            return 'default'
-        if model._meta.app_label == 'bhs':
+        if model._meta.label_lower in models:
             return 'bhs_db'
         return None
 
     def allow_relation(self, obj1, obj2, **hints):
-        """Allow relations if a model in the bhs app is involved."""
+        """Reject relations if a model in the bhs app is involved."""
         models = [
-            'person',
-            'group',
-            'member',
-            'officer',
-            'chart',
-            'repertory',
+            'bhs.human',
+            'bhs.structure',
+            'bhs.status',
+            'bhs.membership',
+            'bhs.subscription',
+            'bhs.role',
+            'bhs.join',
         ]
-        if (obj1._meta.app_label == 'bhs' and obj1._meta.model_name in models) or (obj2._meta.app_label == 'bhs' and obj1._meta.model_name in models):
-            return True
+        if (obj1._meta.label_lower in models) and (obj2._meta.label_lower not in models):
+            return False
         return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         """Make sure the bhs app only appears in the 'bhs_db' database."""
         models = [
-            'person',
-            'group',
-            'member',
-            'officer',
-            'chart',
-            'repertory',
+            'human',
+            'structure',
+            'status',
+            'membership',
+            'subscription',
+            'role',
+            'join',
         ]
-        # if app_label == 'bhs' and model_name in models:
-        #     return True
-        # if app_label == 'bhs':
-        #     return False
-        return True
+        if app_label == 'bhs' and model_name in models:
+            return False
+        return None
