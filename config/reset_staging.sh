eec222de89204137b34eae4964503b274223faa8
David Binetti <dbinetti@gmail.com>
2019-07-28 16:13:12 -0700

    Delete reset_staging.sh

--- config/reset_staging.sh
+++
@@ -1,3 +0,0 @@
-heroku pgbackups:capture --expire --app barberscore-api
-heroku pgbackups:restore DATABASE `heroku pgbackups:url --app barberscore-api` \
---app barberscore-api-staging --confirm barberscore-api-staging
