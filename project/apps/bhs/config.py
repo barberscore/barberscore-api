522817bdcbd86a2cc39c1acf39d6eb4a73130f08
David Binetti <dbinetti@gmail.com>
2019-07-27 18:35:26 -0700

    remove indexing until done with cleanup

--- project/apps/bhs/config.py
+++ project/apps/bhs/config.py
@@ -10,19 +10,19 @@ class BhsConfig(AppConfig):
     def ready(self):
         import algoliasearch_django as algoliasearch
 
-        from .indexes import AwardIndex
-        Award = self.get_model('award')
-        algoliasearch.register(Award, AwardIndex)
+        # from .indexes import AwardIndex
+        # Award = self.get_model('award')
+        # algoliasearch.register(Award, AwardIndex)
 
-        from .indexes import ChartIndex
-        Chart = self.get_model('chart')
-        algoliasearch.register(Chart, ChartIndex)
+        # from .indexes import ChartIndex
+        # Chart = self.get_model('chart')
+        # algoliasearch.register(Chart, ChartIndex)
 
-        from .indexes import GroupIndex
-        Group = self.get_model('group')
-        algoliasearch.register(Group, GroupIndex)
+        # from .indexes import GroupIndex
+        # Group = self.get_model('group')
+        # algoliasearch.register(Group, GroupIndex)
 
-        from .indexes import PersonIndex
-        Person = self.get_model('person')
-        algoliasearch.register(Person, PersonIndex)
+        # from .indexes import PersonIndex
+        # Person = self.get_model('person')
+        # algoliasearch.register(Person, PersonIndex)
         return
