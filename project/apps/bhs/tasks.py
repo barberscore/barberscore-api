b0d555a1319ed7ab1e09a5b0df81bf28b6d684c6
David Binetti <dbinetti@gmail.com>
2019-08-08 14:01:37 -0700

    WIP API updaters

--- project/apps/bhs/tasks.py
+++ project/apps/bhs/tasks.py
@@ -1,5 +1,11 @@
-# from django_rq import job
-# from django.apps import apps
+import requests
+
+from django_rq import job
+from django.apps import apps
+from django.conf import settings
+#
+from .serializers import PersonSerializer
+from .serializers import GroupSerializer
 
 # @job('low')
 # def create_or_update_group_from_structure(structure):
@@ -17,3 +23,26 @@
 # def create_or_update_officer_from_role(role):
 #     Officer = apps.get_model('bhs.officer')
 #     return Officer.objects.update_or_create_from_role(role)
+
+# @job('low')
+def update_person_from_source(person):
+    source_type, _, source_pk = person.source_id.partition("|")
+    if source_type != 'bhs':
+        return
+    response = requests.get('http://localhost:8000/bhs/person/{0}'.format(source_pk))
+    human = response.json()
+    serialized = PersonSerializer(person, data=human['data']['attributes'])
+    if serialized.is_valid():
+        return serialized.save()
+    return serialized.errors
+
+def update_group_from_source(group):
+    source_type, _, source_pk = group.source_id.partition("|")
+    if source_type != 'bhs':
+        return
+    response = requests.get('http://localhost:8000/bhs/group/{0}'.format(source_pk))
+    structure = response.json()
+    serialized = GroupSerializer(group, data=structure['data']['attributes'])
+    if serialized.is_valid():
+        return serialized.save()
+    return serialized.errors
