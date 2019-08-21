2cb57477f7e8b7ce023079b162431486cb9f06e6
David Binetti <dbinetti@gmail.com>
2019-08-21 10:42:27 -0700

    Update sync scripts

--- project/apps/bhs/management/commands/update_membercenter.py
+++ project/apps/bhs/management/commands/update_membercenter.py
@@ -65,88 +65,83 @@ class Command(BaseCommand):
             cursor = None
 
         # Sync Persons
-        self.stdout.write("Fetching Persons from Member Center...")
-        endpoint, _, token = settings.MEMBERCENTER_URL.partition('@')
-        url = "{0}/bhs/person".format(endpoint)
-        headers = {
-            'Authorization': 'Token {0}'.format(token)
-        }
-        page = 1
-        params = {
-            'status': Person.STATUS.active,
-            'modified__gt': cursor,
-            'page': page,
-        }
-        response = requests.get(
-            url,
-            headers=headers,
-            params=params,
-        ).json()
-        t = response['meta']['pagination']['count']
-        if t:
-            i = 0
-            pages = response['meta']['pagination']['pages']
-            while page <= pages:
-                response = requests.get(
-                    url,
-                    headers=headers,
-                    params=params,
-                ).json()
-                items = response['data']
-                for item in items:
-                    i += 1
-                    self.stdout.flush()
-                    self.stdout.write("Updating {0} of {1} Persons...".format(i, t), ending='\r')
-                    update_person_from_membercenter.delay(item)
-                page += 1
-                params['page'] = page
-            self.stdout.write("")
-        self.stdout.write("Updated {0} Persons.".format(t))
-        # if not cursor:
-        #     humans = list(Human.objects.values_list('id', flat=True))
-        #     self.stdout.write("Deleting Person orphans...")
-        #     t = Person.objects.delete_orphans(humans)
-        #     self.stdout.write("Deleted {0} Person orphans.".format(t))
+        # self.stdout.write("Fetching Persons from Member Center...")
+        # endpoint, _, token = settings.MEMBERCENTER_URL.partition('@')
+        # url = "{0}/bhs/person".format(endpoint)
+        # headers = {
+        #     'Authorization': 'Token {0}'.format(token)
+        # }
+        # page = 1
+        # params = {
+        #     'status': Person.STATUS.active,
+        #     'modified__gt': cursor,
+        #     'page': page,
+        # }
+        # response = requests.get(
+        #     url,
+        #     headers=headers,
+        #     params=params,
+        # ).json()
+        # t = response['meta']['pagination']['count']
+        # if t:
+        #     i = 0
+        #     pages = response['meta']['pagination']['pages']
+        #     while page <= pages:
+        #         response = requests.get(
+        #             url,
+        #             headers=headers,
+        #             params=params,
+        #         ).json()
+        #         items = response['data']
+        #         for item in items:
+        #             i += 1
+        #             self.stdout.flush()
+        #             self.stdout.write("Updating {0} of {1} Persons...".format(i, t), ending='\r')
+        #             update_person_from_membercenter.delay(item)
+        #         page += 1
+        #         params['page'] = page
+        #     self.stdout.write("")
+        # self.stdout.write("Updated {0} Persons.".format(t))
 
         # Sync Groups
-        self.stdout.write("Fetching Groups from Member Center...")
-        endpoint, _, token = settings.MEMBERCENTER_URL.partition('@')
-        url = "{0}/bhs/group".format(endpoint)
-        headers = {
-            'Authorization': 'Token {0}'.format(token)
-        }
-        page = 1
-        params = {
-            'status': Group.STATUS.active,
-            'kind__gt': 30,
-            'modified__gt': cursor,
-            'page': page,
-        }
-        response = requests.get(
-            url,
-            headers=headers,
-            params=params,
-        ).json()
-        t = response['meta']['pagination']['count']
-        if t:
-            i = 0
-            pages = response['meta']['pagination']['pages']
-            while page <= pages:
-                response = requests.get(
-                    url,
-                    headers=headers,
-                    params=params,
-                ).json()
-                items = response['data']
-                for item in items:
-                    i += 1
-                    self.stdout.flush()
-                    self.stdout.write("Updating {0} of {1} Groups...".format(i, t), ending='\r')
-                    update_group_from_membercenter.delay(item)
-                page += 1
-                params['page'] = page
-            self.stdout.write("")
-        self.stdout.write("Updated {0} Groups.".format(t))
+        # self.stdout.write("Fetching Groups from Member Center...")
+        # endpoint, _, token = settings.MEMBERCENTER_URL.partition('@')
+        # url = "{0}/bhs/group".format(endpoint)
+        # headers = {
+        #     'Authorization': 'Token {0}'.format(token)
+        # }
+        # page = 1
+        # params = {
+        #     'status': Group.STATUS.active,
+        #     'kind__gt': 30,
+        #     'modified__gt': cursor,
+        #     'page': page,
+        # }
+        # response = requests.get(
+        #     url,
+        #     headers=headers,
+        #     params=params,
+        # ).json()
+        # t = response['meta']['pagination']['count']
+        # if t:
+        #     i = 0
+        #     pages = response['meta']['pagination']['pages']
+        #     while page <= pages:
+        #         response = requests.get(
+        #             url,
+        #             headers=headers,
+        #             params=params,
+        #         ).json()
+        #         items = response['data']
+        #         for item in items:
+        #             i += 1
+        #             self.stdout.flush()
+        #             self.stdout.write("Updating {0} of {1} Groups...".format(i, t), ending='\r')
+        #             update_group_from_membercenter.delay(item)
+        #         page += 1
+        #         params['page'] = page
+        #     self.stdout.write("")
+        # self.stdout.write("Updated {0} Groups.".format(t))
 
         # Sync Roles
         self.stdout.write("Fetching Officers from Member Center...")
--- project/apps/bhs/tasks.py
+++ project/apps/bhs/tasks.py
@@ -53,38 +53,6 @@ def get_membercenter_token():
     return membercenter_api_access_token
 
 
-def get_membercenter_response(source):
-    source_type, _, source_pk = source.source_id.partition("|")
-    if source_type != 'bhs':
-        return ValueError("Source must be BHS")
-    endpoint, _, token = settings.MEMBERCENTER_URL.partition('@')
-    url = "{0}/bhs/{1}/{2}".format(
-        endpoint,
-        source._meta.model_name,
-        source_pk,
-    )
-    headers = {
-        'Authorization': 'Token {0}'.format(token)
-    }
-    response = requests.get(
-        url,
-        headers=headers,
-    )
-    return response
-
-@job('low')
-def update_person_from_source(person):
-    User = get_user_model()
-    response = get_membercenter_response(person)
-    human = response.json()
-    parsed = JSONParser().parse_attributes(human['data'])
-    serialized = PersonSerializer(person, data=parsed)
-    if serialized.is_valid():
-        usernames = parsed['usernames']
-        owners = User.objects.filter(username__in=usernames).values_list('id', flat=True)
-        return serialized.save(owners=owners)
-    return serialized.errors
-
 @job('low')
 def update_person_from_membercenter(resource):
     Person = apps.get_model('bhs.person')
@@ -105,6 +73,7 @@ def update_person_from_membercenter(resource):
         return serialized.save(owners=owners)
     raise ValueError(serialized.errors)
 
+
 @job('low')
 def update_group_from_membercenter(resource):
     Group = apps.get_model('bhs.group')
@@ -124,6 +93,7 @@ def update_group_from_membercenter(resource):
         owners = User.objects.filter(username__in=usernames).values_list('id', flat=True)
         return serialized.save(owners=owners)
     raise ValueError(serialized.errors)
+
 
 @job('low')
 def update_group_owners_from_membercenter(resource):
@@ -152,93 +122,3 @@ def update_group_owners_from_membercenter(resource):
             return group.owners.add(user)
         return group.owners.remove(user)
     return
-
-
-# def get_or_create_account_from_email(email):
-#     auth0 = get_auth0()
-#     results = auth0.users_by_email.search_users_by_email(email)
-#     if results:
-#         account = results[0]
-#         created = False
-#     else:
-#         password = get_random_string()
-#         payload = {
-#             'connection': 'Default',
-#             'email': email,
-#             'email_verified': True,
-#             'password': password,
-#         }
-#         account = auth0.users.create(payload)
-#         created = True
-#     return account, created
-
-
-# @job('low')
-# def update_account_from_user(user):
-#     auth0 = get_auth0()
-#     payload = model_to_dict(
-#         user,
-#         fields=[
-#             'email',
-#             'name',
-#             'first_name',
-#             'last_name',
-#             'app_metadata',
-#             'user_metadata',
-#         ]
-#     )
-#     # Re-write payload to conform to Auth0
-#     if user.image_url:
-#         payload['picture'] = user.image_url
-#     payload['given_name'] = payload.pop('first_name')
-#     payload['family_name'] = payload.pop('last_name')
-#     return auth0.users.update(user.username, payload)
-
-
-# @job('low')
-# def delete_account_from_user(user):
-#     auth0 = get_auth0()
-#     return auth0.users.delete(user.username)
-
-# @job('low')
-# def add_account_roles_from_user_pk_set(user, role, pk_set):
-#     auth0 = get_auth0()
-#     roles_raw = role.objects.filter(id__in=pk_set)
-#     roles = [str(i.rolename) for i in roles_raw]
-#     return auth0.users.add_roles(user.username, roles)
-
-# @job('low')
-# def remove_account_roles_from_user_pk_set(user, role, pk_set):
-#     auth0 = get_auth0()
-#     roles_raw = role.objects.filter(id__in=pk_set)
-#     roles = [str(i.rolename) for i in roles_raw]
-#     return auth0.users.remove_roles(user.username, roles)
-
-
-
-
-
-
-
-
-
-
-
-
-# @job('low')
-# def create_or_update_group_from_structure(structure):
-#     Group = apps.get_model('bhs.group')
-#     return Group.objects.update_or_create_from_structure(structure)
-
-
-# @job('low')
-# def create_or_update_person_from_human(human):
-#     Person = apps.get_model('bhs.person')
-#     return Person.objects.update_or_create_from_human(human)
-
-
-# @job('low')
-# def create_or_update_officer_from_role(role):
-#     Officer = apps.get_model('bhs.officer')
-#     return Officer.objects.update_or_create_from_role(role)
-
