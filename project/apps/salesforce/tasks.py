01946cbadf0fd688b979b9705e52a5da5dd0047a
Alex Rubin <alex@webgraph.com>
2021-07-02 19:41:30 -0400

    Updated Tasks to auto requeue Contests and Contest Entries.

--- project/apps/salesforce/tasks.py
+++ project/apps/salesforce/tasks.py
@@ -6,11 +6,12 @@ import logging
 import time
 
 # Third-Party
-from django_rq import job
+from django_rq import job, get_queue
 
 # Django
 from apps.bhs.models import Convention, Award, Chart, Group, Person
 from apps.registration.models import Contest, Session, Assignment, Entry
+from django.core.exceptions import ObjectDoesNotExist
 
 log = logging.getLogger(__name__)
 
@@ -39,9 +40,23 @@ def update_or_create_session_from_salesforce(session):
     return Session.objects.update_or_create_session(session)
 
 @job('high')
-def update_or_create_contest_from_salesforce(contest):
-    time.sleep(20)
-    return Contest.objects.update_or_create_contest(contest)
+def update_or_create_contest_from_salesforce(contest, max_retries=10):
+    queue = get_queue('high')
+
+    # print('Search for SessionID + ' + contest['session_id'])
+
+    #
+    # Query Session to see if session_id record exists
+    # 
+
+    if max_retries > 0:
+        if Session.objects.filter(pk=contest['session_id']).count():
+            return Contest.objects.update_or_create_contest(contest)
+        else :
+            time.sleep(60)
+            queue.enqueue(update_or_create_contest_from_salesforce, args=(contest, (max_retries - 1)))
+    else:
+        raise ObjectDoesNotExist("Session ID not found: " + contest['session_id'])
 
 @job('high')
 def update_or_create_assignment_from_salesforce(assignment):
@@ -52,13 +67,31 @@ def update_or_create_entry_from_salesforce(entry):
     return Entry.objects.update_or_create_entry(entry)
 
 @job('high')
-def update_contest_entry_from_salesforce(entry):
-    time.sleep(30)
-    return Entry.objects.update_contestentry_status(entry)
+def update_contest_entry_from_salesforce(entry, max_retries=10):
+    queue = get_queue('high')
+
+    # print('Search for EntryID + ' + entry['entry_id'])
+
+    #
+    # Query Entry to see if entry_id record exists
+    # 
+
+    if max_retries > 0:
+        if Entry.objects.filter(pk=entry['entry_id']).count():
+            return Entry.objects.update_contestentry_status(entry)
+        else :
+            time.sleep(60)
+            queue.enqueue(update_contest_entry_from_salesforce, args=(entry, (max_retries - 1)))
+    else:
+        raise ObjectDoesNotExist("Entry ID not found: " + entry['entry_id'])
+
 
 @job('high')
 def update_group_chart_from_salesforce(chart):
 	return Group.objects.update_group_chart(chart)
+
+
+# Delete Jobs
 
 @job('high')
 def delete_convention(uuid):
